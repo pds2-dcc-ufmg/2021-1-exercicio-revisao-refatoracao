f09d839fa9511e65062705b331ba890b4c024ed7
igorlfs <igorlfs@ufmg.br>
2021-08-08 16:49:18 -0300

    [Imovel] construtor; valor()

--- include/Imovel.hpp
+++ include/Imovel.hpp
@@ -9,17 +9,29 @@ using namespace std;
 
 class Imovel {
 public:
+  string corretor;
   double area;
-  int numQuartos;
-  int numBanheiros;
-  int vagas;
+  unsigned numQuartos;
+  unsigned numBanheiros;
+  unsigned vagas;
   double valorPorArea;
   double valorVenda;
   double valorComissao;
   Cliente vendedor;
-  string corretor;
 
   void print() const;
+  double valor() const { return this->area * this->valorPorArea; }
+  virtual double comissao() const;
+
+  Imovel(const string &corretor, const double &area, const unsigned &numQuartos,
+         const unsigned &numBanheiros, const unsigned &vagas,
+         const double &valorPorArea, const Cliente &vendedor)
+      : corretor(corretor), area(area), numQuartos(numQuartos),
+        numBanheiros(numBanheiros), vagas(vagas), valorPorArea(valorPorArea),
+        vendedor(vendedor) {
+    this->valorComissao = this->comissao();
+    this->valorVenda = this->comissao() + this->valor();
+  }
 };
 
 #endif
