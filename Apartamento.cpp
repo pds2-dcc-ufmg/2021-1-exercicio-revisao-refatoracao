988d01d953ad0489304689f98cc2a378c35dab67
root <root@DESKTOP-7SG36SN.localdomain>
2021-08-11 02:13:07 -0300

    Arquivos adicionados

---
+++ Apartamento.cpp
@@ -0,0 +1,25 @@
+#include "Apartamento.hpp"
+
+double valor() {
+
+    return AREA * VALORm2;
+}
+
+double comissao() {
+
+    return valor()*TAXA_COMISSAO; 
+}
+
+void print() {
+
+    std::cout << "[Apartamento]" << endl;
+    Imovel::print();
+    std::cout << "Area: " << AREA << endl
+            << "  Quartos: " << Q << endl
+            << "  Banheiros: " << B << endl
+            << "  Vagas: " << V << endl
+            << "Taxa de Comissão: " << 4 << "%" << endl
+            << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
+            << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
+    }
+}
