8eab6bc8898a075f13b8be24f8f5aa09d4d43f75
Lucas Werlang <lucaswewe2806@gmail.com>
2021-08-12 11:34:27 -0300

    Update Casa.hpp
    mudancas em casa.hpp para tirar o numero magico

--- Casa.hpp
+++ Casa.hpp
@@ -1,6 +1,6 @@
 #ifndef CASA_HPP
 #define CASA_HPP
-
+#define seisporcento 0.06
 #include "Imovel.hpp"
 #include "Cliente.hpp"
 
@@ -22,7 +22,7 @@ class Casa : public Imovel {
 
         double c = AREA * VALORm2;
 
-        return c * 0.06;
+        return c * seisporcento;
         
     }
 
@@ -31,9 +31,9 @@ class Casa : public Imovel {
         std::cout << "[Casa]" << endl;
         Imovel::print();
         std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
+                << "  Quartos: " << QUARTOS << endl
+                << "  Banheiros: " << BANHEIROS << endl
+                << "  Vagas: " << VAGAS << endl
                 << "Taxa de Comissão: " << 6 << "%" << endl
                 << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
                 << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
