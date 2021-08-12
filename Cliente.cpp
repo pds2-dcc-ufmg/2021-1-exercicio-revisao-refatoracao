865928b7a184d21663d790c3d5cdaffaa4035367
Matheus <matheusmiranda98@live.com>
2021-08-12 18:58:54 -0300

    Versão final
    Versão com todas as mudanças

--- Cliente.cpp
+++ Cliente.cpp
@@ -2,13 +2,23 @@
 #include <string>
 #include "Cliente.hpp"
 
-void Cliente::print(){
+Cliente::Cliente(const char *nome, const char *end, const char *cid, const char *unifed, const char *cp, const char *tel)
+{
+	NOME = nome;
+	endereco = end;
+	CIDADE = cid;
+	UF = unifed;
+	cep = cp;
+	telefone = tel;
+}
+
+void Cliente::print()
+{
 
 	std::cout << "  Nome: " << NOME << endl
-		 << "  Telefone: " << telefone << endl
-		 << "  Endereço: " << endereco << endl
-		 << "  Cidade: " << CIDADE << endl
-		 << "  Estado: " << UF << endl
-		 << "  CEP: " << cep << endl;
-
+			  << "  Telefone: " << telefone << endl
+			  << "  Endereço: " << endereco << endl
+			  << "  Cidade: " << CIDADE << endl
+			  << "  Estado: " << UF << endl
+			  << "  CEP: " << cep << endl;
 }
--- Imovel.hpp
+++ Imovel.hpp
@@ -7,24 +7,99 @@
 
 using namespace std;
 
-class Imovel {
-    public:
-        double AREA;
-        int Q;
-        int B;
-        int V;
-        double VALORm2;
-        double Valor;
-        double C;
-        Cliente vendedor;
-        string corretor;
+class Imovel
+{
+public:
 
-        void print() {
-            cout << "[Vendedor]" << endl;
-            vendedor.print();
-            cout << "[Corretor]" << endl;
-            cout << "  " + corretor << endl;
-        }
+    //Exibe os dados do cliente e o nome do corretor
+    void print();
+
+    //Calcula o valor do imovel
+    double valor();
+
+    // Funções de Get
+    double getAreaDoImovel();
+    double getValorMetroQuadrado();
+    double getValorDeVenda();
+    double getValorComissao();
+    int getQuartos();
+    int getBanheiros();
+    int getVagas();
+
+    // Funções de Set
+    void setAreaDoImovel(double setA);
+    void setValorMetroQuadrado(double setVm2);
+    void setValorDeVenda(double setVDV);
+    void setValorComissao(double setVC);
+    void setQuartos(double setQ);
+    void setBanheiros(double setB);
+    void setVagas(double setV);
+
+protected:
+    //Cliente da Corretora que está vendendo o imovel
+    Cliente vendedor;
+
+    //Nome do corretor
+    string corretor;
+
+    //Valores do imovel
+    double AreaDoImovel = 0;
+    double ValorMetroQuadrado = 0;
+    double ValorDeVenda = 0;
+    double ValorComissao = 0;
+
+    //Dados do imovel
+    int Quartos = 0;
+    int Banheiros = 0;
+    int Vagas = 0;
+
+    //Taxas por tipo de imovel
+    double apartamento_taxa = 0.04;
+    double casa_taxa = 0.06;
+    double cobertura_taxa = 0.10;
+
+};
+
+class Apartamento : public Imovel
+{
+
+public:
+    //Construtor de apartamento
+    Apartamento(string nome, double ADI, double VMQ, int Q, int B, int V, Cliente cl);
+
+    //Calcula o valor da comissao
+    double comissao();
+
+    //Printa os dados do imovel
+    void print();
+};
+
+class Casa : public Imovel
+{
+
+public:
+    //Construtor de Casa
+    Casa(string nome, double ADI, double VMQ, int Q, int B, int V, Cliente cl);
+
+    //Calcula o valor da comissao
+    double comissao();
+
+    //Printa os dados do imovel
+    void print();
+};
+
+class Cobertura : public Imovel
+{
+
+public:
+    //Construtor de Cobertura
+    Cobertura(string nome, double ADI, double VMQ, int Q, int B, int V, Cliente cl);
+
+    //Calcula o valor da comissao
+    double comissao();
+
+    //Printa os dados do imovel
+    void print();
 };
 
 #endif
