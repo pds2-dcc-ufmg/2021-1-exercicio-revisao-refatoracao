debd14250996cf136e803d909900c2a8b48c31c0
limab300 <gabriellbarros-@hotmail.com>
2021-08-12 10:32:24 -0300

    Resolução Exercício Revisão e Refatoração

--- Apartamento.hpp
+++ Apartamento.hpp
@@ -7,37 +7,27 @@
 using namespace std;
 
 class Apartamento : public Imovel {
-
-    public: 
-    
-    double valor() {
-
-        double v = AREA * VALORm2;
-
-        return v;
-
-    }
-
-    double comissao() {
-
-        double c = AREA * VALORm2;
-
-        return c * 0.04;
-        
-    }
-
-    void print() {
-
-        std::cout << "[Apartamento]" << endl;
-        Imovel::print();
-        std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
-                << "Taxa de Comissão: " << 4 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
-                << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
-    }
+ public:
+    // Retorna o valor total do imóvel sem a comissão.
+        double valor_imovel() {
+            double v = area * valor_m2;
+            return v;
+        }
+    // Retorna o valor da comissão.
+        double comissao() {
+            double c = area * valor_m2;
+            return c * 0.04;
+        }
+        int get_porcentagem() {
+            return porcentagem;
+        }
+    // Retorna todas as informações do imóvel em questão.
+        void print() {
+            std::cout << "[Apartamento]" << endl;
+            Imovel::print();
+        }
+    private:
+        int porcentagem = 4;
 };
 
 #endif
