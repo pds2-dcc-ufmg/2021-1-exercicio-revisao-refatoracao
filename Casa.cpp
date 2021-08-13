2e666b5a848066cb933a0551f50c462dd6450e7e
Joao Pedro Reis <jpreis808@gmail.com>
2021-08-12 22:15:12 -0300

    Move method bodies to .cpp files

---
+++ Casa.cpp
@@ -0,0 +1,23 @@
+#include "Casa.hpp"
+
+double Casa::valor() {
+    double v = AREA * VALORm2;
+    return v;
+}
+
+double Casa::comissao() {
+    double c = AREA * VALORm2;
+    return c * 0.06;
+}
+
+void Casa::print() {
+    std::cout << "[Casa]" << endl;
+    Imovel::print();
+    std::cout << "Area: " << AREA << endl
+            << "  Quartos: " << Q << endl
+            << "  Banheiros: " << B << endl
+            << "  Vagas: " << V << endl
+            << "Taxa de Comissão: " << 6 << "%" << endl
+            << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
+            << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
+}
---
+++ Cobertura.cpp
@@ -0,0 +1,23 @@
+#include "Cobertura.hpp"
+
+double Cobertura::valor() {
+    double v = AREA * VALORm2;
+    return v;
+}
+
+double Cobertura::comissao() {
+    double c = AREA * VALORm2;
+    return c * 0.10;  
+}
+
+void Cobertura::print() {
+    std::cout << "[Cobertura]" << endl;
+    Imovel::print();
+    std::cout << "Area: " << AREA << endl
+            << "  Quartos: " << Q << endl
+            << "  Banheiros: " << B << endl
+            << "  Vagas: " << V << endl
+            << "Taxa de Comissão: " << 10 << "%" << endl
+            << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
+            << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
+}
