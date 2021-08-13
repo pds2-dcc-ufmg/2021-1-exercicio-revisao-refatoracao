2bf72305e8d24daadb4b5bcbbb4c0b2c12128bdf
Victor Batista <victor.guedes.08@gmail.com>
2021-08-13 16:21:39 -0300

    fix

--- include/Cliente.hpp
+++ include/Cliente.hpp
@@ -9,11 +9,17 @@ class Cliente {
     public:
 
         void print();
+
         void setNome(string n);
+        
         void setEndereco(string e);
+
         void setCidade(string c);
+
         void setUF(string U);
+
         void setCEP(string C);
+        
         void setTelefone(string t);
     
     private:
