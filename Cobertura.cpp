353a77a7dbf3539dba9a2c03ffcc3cf878e9de68
Beatiz Vocurca Frade <beatrizvocurafrade@gmail.com>
2021-08-13 12:49:33 -0300

    adicionando arquivos

---
+++ Cobertura.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Cobertura.hpp"
+#include "ImovelProps.hpp"
+#include "Imovel.hpp"
+
+int Cobertura::_somaQte = 0;
+double Cobertura::_somaValor = 0;
+double Cobertura::_somaComissao = 0;
+
+void Cobertura::print () {
+  std::cout << "[Cobertura]" << endl;
+
+  printTituloImovel();
+  printImovel();
+}
+
+Cobertura::Cobertura (ImovelProps props) :
+  Imovel(props) {
+    _somaQte++;
+    _somaValor += getValor();
+    _somaComissao += getComissao();
+}
