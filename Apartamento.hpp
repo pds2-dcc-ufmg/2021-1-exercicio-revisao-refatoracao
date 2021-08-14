36913ddcb6dbe8a44311c99aea336f071a722ccb
Artur <arthur.gaspar2011@gmail.com>
2021-08-14 14:53:48 -0300

    Adicionado Apartamento.cpp

--- Apartamento.hpp
+++ Apartamento.hpp
@@ -19,44 +19,13 @@ class Apartamento : public Imovel {
             double valor_m2,
             Cliente vendedor,
             string corretor
-        ):Imovel(
-            area,
-            quartos,
-            banheiros,
-            vagas,
-            valor_m2,
-            vendedor,
-            corretor
-        ){}
+        );
 
-        double valor() override {
+        double valor() override;
 
-            double valorTotal = area * valor_m2;
+        double comissao() override;
 
-            return valorTotal;
-
-        }
-
-        double comissao() override {
-
-            double resultado = area * valor_m2;
-
-            return resultado * taxa_comissao;
-            
-        }
-
-        void print() {
-
-            std::cout << "[Apartamento]" << endl;
-            Imovel::print();
-            std::cout << "Area: " << area << endl
-                    << "  Quartos: " << quartos << endl
-                    << "  Banheiros: " << banheiros << endl
-                    << "  Vagas: " << vagas << endl
-                    << "Taxa de Comissão: " << taxa_comissao*100 << "%" << endl
-                    << "Valor Comissão: R$ " << fixed << setprecision(2) << comissao_imovel << endl
-                    << "Valor de Venda: R$ " << fixed << setprecision(2) << valor_total << endl;
-        }
+        void print();
 };
 
 #endif
