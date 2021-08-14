81b73d354a1162437f60dfe7ed15038b3bf1539a
Mateusmsc3 <86414375+Mateusmsc3@users.noreply.github.com>
2021-08-14 17:56:31 -0300

    declarando do metodo print()

--- Apartamento.hpp
+++ Apartamento.hpp
@@ -12,18 +12,7 @@ class Apartamento : public Imovel {
 
     public: 
 
-    void print() {
-
-        std::cout << "[Apartamento]" << endl;
-        Imovel::print();
-        std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
-                << "Taxa de Comissão: " << 4 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
-                << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
-    }
+    
 };
 
 #endif
