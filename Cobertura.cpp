04456ea9a727c27b110833f6a359ceac63623981
Mateusmsc3 <86414375+Mateusmsc3@users.noreply.github.com>
2021-08-14 18:30:19 -0300

    Implementação do construtor da classe Cobertura

--- Cobertura.cpp
+++ Cobertura.cpp
@@ -2,3 +2,8 @@
 #include <string>
 #include "Cliente.hpp"
 #include "Cobertura.hpp"
+
+Cobertura::Cobertura(string cor, double a, int q, int b, int vag, double valm2, Cliente ven): Imovel(cor, a, q, b, vag, valm2, ven){
+    _porcComissao = 0.1;
+    _taxa = 10;
+};
