af1049f83247c660d571ecda97d59bcb4478215a
Mateusmsc3 <86414375+Mateusmsc3@users.noreply.github.com>
2021-08-14 18:10:59 -0300

    declaração do método print()
    implementação será feita em Casa.cpp

--- Casa.hpp
+++ Casa.hpp
@@ -12,21 +12,8 @@ class Casa : public Imovel {
 
     public:
 
-
-    void print() {
-
-        std::cout << "[Casa]" << endl;
-        Imovel::print();
-        std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
-                << "Taxa de Comissão: " << 6 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
-                << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
-
-    }
-
+    void print();
+    
 };
 
 #endif
