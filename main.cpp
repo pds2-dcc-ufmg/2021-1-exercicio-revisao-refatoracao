8ae4bd44523813d1e68825da321f103ff8dc62d5
Mateusmsc3 <86414375+Mateusmsc3@users.noreply.github.com>
2021-08-14 17:14:55 -0300

    implementação de mais contrutores
    utilização de construtores para as classes Apartamento, Casa, e Cobertura

--- main.cpp
+++ main.cpp
@@ -69,102 +69,30 @@ int main() {
     int qtdeCa = 0;
     int qtdeCb = 0;
 
-    Apartamento ap1;
-    ap1.corretor = "Tuoruars";
-    ap1.AREA = 55.4;
-    ap1.Q = 2;
-    ap1.B = 1;
-    ap1.V = 0;
-    ap1.VALORm2 = 987.0;
-    ap1.C = ap1.comissao();
-    ap1.Valor = ap1.valor() + ap1.comissao();
-    ap1.vendedor = cl1;
+     Apartamento ap1 = Apartamento("Tuoruars", 55.4, 2, 1, 0, 987.0, cl1);
     qtdeA++;
 
-    Apartamento ap2;
-    ap2.corretor = "Fyubyeis";
-    ap2.AREA = 74.5;
-    ap2.Q = 2;
-    ap2.B = 1;
-    ap2.V = 2;
-    ap2.VALORm2 = 1540.0;
-    ap2.C = ap2.comissao();
-    ap2.Valor = ap2.valor() + ap2.comissao();
-    ap2.vendedor = cl2;
+    Apartamento ap2 = Apartamento("Fyubyeis", 74.5, 2, 1, 2, 1540.0, cl2);
     qtdeA++;
 
-    Apartamento ap3;
-    ap3.corretor = "Kelia";
-    ap3.AREA = 87.2;
-    ap3.Q = 3;
-    ap3.B = 2;
-    ap3.V = 2;
-    ap3.VALORm2 = 2354.0;
-    ap3.C = ap3.comissao();
-    ap3.Valor = ap3.valor() + ap3.comissao();
-    ap3.vendedor = cl3;
+    Apartamento ap3 = Apartamento("Kelia", 87.2, 3, 2, 2, 2354.0, cl3);
     qtdeA++;
 
     // Cobertura
-    Cobertura cb1;
-    cb1.corretor = "Koci";
-    cb1.AREA = 120.1;
-    cb1.Q = 3;
-    cb1.B = 3;
-    cb1.V = 2;
-    cb1.VALORm2 = 3123.5;
-    cb1.C = cb1.comissao();
-    cb1.Valor = cb1.valor() + cb1.comissao();
-    cb1.vendedor = cl4;
-    qtdeCb++;
-    
-    Cobertura cb2;
-    cb2.corretor = "Wail";
-    cb2.AREA = 134.8;
-    cb2.Q = 4;
-    cb2.B = 3;
-    cb2.V = 3;
-    cb2.VALORm2 = 3578.2;
-    cb2.C = cb2.comissao();
-    cb2.Valor = cb2.valor() + cb2.comissao();
-    cb2.vendedor = cl5;
+    Cobertura cb1 = Cobertura("Koci", 120.1, 3, 3, 2, 3123.5, cl4);
     qtdeCb++;
 
-    Cobertura cb3;
-    cb3.corretor = "Fival";
-    cb3.AREA = 180.0;
-    cb3.Q = 4;
-    cb3.B = 4;
-    cb3.V = 4;
-    cb3.VALORm2 = 4165.7;
-    cb3.C = cb3.comissao();
-    cb3.Valor = cb3.valor() + cb3.comissao();
-    cb3.vendedor = cl6;
+    Cobertura cb2 = Cobertura("Wail", 134.8, 4, 3, 3, 3578.2, cl5);
+    qtdeCb++;
+
+    Cobertura cb3 = Cobertura("Fival", 180.0, 4, 4, 4, 4165.7, cl6);
     qtdeCb++;
 
     // Casas
-    Casa ca1;
-    ca1.corretor = "Beydo";
-    ca1.AREA = 145.6;
-    ca1.Q = 3;
-    ca1.B = 3;
-    ca1.V = 2;
-    ca1.VALORm2 = 4023.6;
-    ca1.C = ca1.comissao();
-    ca1.Valor = ca1.valor() + ca1.comissao();
-    ca1.vendedor = cl7;
+    Casa ca1 = Casa("Beydo", 145.6, 3, 3, 2, 4023.6, cl7);
     qtdeCa++;
 
-    Casa ca2;
-    ca2.corretor = "Riuzi";
-    ca2.AREA = 245.0;
-    ca2.Q = 5;
-    ca2.B = 4;
-    ca2.V = 4;
-    ca2.VALORm2 = 4856.2;
-    ca2.C = ca2.comissao();
-    ca2.Valor = ca2.valor() + ca2.comissao();
-    ca2.vendedor = cl8;
+    Casa ca2 = Casa("Riuzi", 245.0, 5, 4, 4, 4856.2, cl8);
     qtdeCa++;
 
     std::cout << "\n>> Relatório de Imóveis <<" << endl;
