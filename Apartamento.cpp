baba2546bab45d6710ff469aeb90471c828c7dd9
Mateusmsc3 <86414375+Mateusmsc3@users.noreply.github.com>
2021-08-14 18:54:01 -0300

    retirada numeros magicos e comentarios

--- Apartamento.cpp
+++ Apartamento.cpp
@@ -3,9 +3,12 @@
 #include "Cliente.hpp"
 #include "Apartamento.hpp"
 
+#define PORCENT_COMISSAO 0.04
+#define TAXA_COMISSAO 4
+
 Apartamento::Apartamento(string cor, double a, int q, int b, int vag, double valm2, Cliente ven): Imovel(cor, a, q, b, vag, valm2, ven){
-    _porcComissao = 0.04;
-    _taxa = 4;
+    _porcComissao = PORCENT_COMISSAO;//variavel que armazena a porcentagem da comissao para apartamentos
+    _taxa = TAXA_COMISSAO;// variavel que armazena a taxa de comissao para apartamentos 
 };
 
 void Apartamento::print() {
