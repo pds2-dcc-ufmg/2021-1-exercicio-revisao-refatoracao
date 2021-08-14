5e8582581cf3e675b72593ed200912e076e8a2c7
Mateusmsc3 <86414375+Mateusmsc3@users.noreply.github.com>
2021-08-14 18:16:05 -0300

    implementação do construtor da classe Casa

--- Casa.cpp
+++ Casa.cpp
@@ -2,3 +2,8 @@
 #include <string>
 #include "Cliente.hpp"
 #include "Casa.hpp"
+
+Casa::Casa(string cor, double a, int q, int b, int vag, double valm2,  Cliente ven): Imovel(cor, a, q, b, vag, valm2, ven){
+    _porcComissao = 0.06;
+    _taxa = 6;
+};
