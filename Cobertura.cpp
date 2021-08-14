344fe5336e904a1cb3468c9a9b74822591f70df4
Mateusmsc3 <86414375+Mateusmsc3@users.noreply.github.com>
2021-08-14 18:59:22 -0300

    Update Cobertura.cpp

--- Cobertura.cpp
+++ Cobertura.cpp
@@ -7,8 +7,8 @@
 #define TAXA_COMISSAO 10
 
 Cobertura::Cobertura(string cor, double a, int q, int b, int vag, double valm2, Cliente ven): Imovel(cor, a, q, b, vag, valm2, ven){
-    _porcComissao = PORCENT_COMISSAO;//variavel que armazena a porcentagem da comissao para apartamentos
-    _taxa = TAXA_COMISSAO;// variavel que armazena a taxa de comissao para apartamentos
+    _porcComissao = PORCENT_COMISSAO;//variavel que armazena a porcentagem da comissao para coberturas
+    _taxa = TAXA_COMISSAO;// variavel que armazena a taxa de comissao para coberturas
 };
 
 void Cobertura::print() {
