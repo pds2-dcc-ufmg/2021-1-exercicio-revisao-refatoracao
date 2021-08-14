02495958332851ee80de9f8ab12375c962d6728a
Mateusmsc3 <86414375+Mateusmsc3@users.noreply.github.com>
2021-08-14 18:02:29 -0300

    implementação do construtor da classe apartamento

--- Apartamento.cpp
+++ Apartamento.cpp
@@ -2,3 +2,8 @@
 #include <string>
 #include "Cliente.hpp"
 #include "Apartamento.hpp"
+
+Apartamento::Apartamento(string cor, double a, int q, int b, int vag, double valm2, Cliente ven): Imovel(cor, a, q, b, vag, valm2, ven){
+    _porcComissao = 0.04;
+    _taxa = 4;
+};
