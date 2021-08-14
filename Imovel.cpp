432c2e00c561268ec7048e9d1436ffad813c7709
Gustavo Cunha <gustavocunha182020@gmail.com>
2021-08-14 16:32:49 -0300

    Refatoração.

---
+++ Imovel.cpp
@@ -0,0 +1,8 @@
+#include "Imovel.hpp"
+
+void Imovel::print() {
+            cout << "[Vendedor]" << endl;
+            vendedor.print();
+            cout << "[Corretor]" << endl;
+            cout << "  " + corretor << endl;
+        }
