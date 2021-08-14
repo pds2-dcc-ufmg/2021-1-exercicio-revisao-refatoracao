56dd95cb027580e6230bd0af8f0f91053398b32e
Mateusmsc3 <86414375+Mateusmsc3@users.noreply.github.com>
2021-08-14 17:48:00 -0300

    Adicionando construtor da classe Imovel

--- Imovel.cpp
+++ Imovel.cpp
@@ -1,1 +1,7 @@
+#include "Cliente.hpp"
+#include "Apartamento.hpp"
 
+Imovel::Imovel(string cor, double a, int q, int b, int vag, double valm2, Cliente ven): _corretor(cor), _area(a), _quartos(q), _banheiros(b), _vagas(vag), _valorm2(valm2), _vendedor(ven){
+    _comissao = comissao();
+    _valor = valor() + comissao();
+};
