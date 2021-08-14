6b0afb2e7499592f3cee6c2b4dc23e676875c9bb
Mateusmsc3 <86414375+Mateusmsc3@users.noreply.github.com>
2021-08-14 18:26:03 -0300

    retirada dos metodos valor() e comissao()
    serão utilizados os métodos valor() e comissao() herdados de Imovel

--- Cobertura.hpp
+++ Cobertura.hpp
@@ -11,23 +11,6 @@ using std::endl;
 class Cobertura : public Imovel {
     
     public:
-
-    double valor() {
-
-        double v = AREA * VALORm2;
-
-        return v;
-
-    }
-
-    double comissao() {
-
-        double c = AREA * VALORm2;
-
-        return c * 0.10;
-        
-    }
-
     void print() {
 
         std::cout << "[Cobertura]" << endl;
