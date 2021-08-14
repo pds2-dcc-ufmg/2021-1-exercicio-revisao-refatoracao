05b51fd425e03e4670e9dd7bbbaa4aeb855af715
Mateusmsc3 <86414375+Mateusmsc3@users.noreply.github.com>
2021-08-14 17:29:48 -0300

    adicionando construtor da classe Cliente no cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -3,12 +3,12 @@
 #include "Cliente.hpp"
 
 void Cliente::print(){
-
-	std::cout << "  Nome: " << NOME << endl
+	cout << "  Nome: " << NOME << endl
 		 << "  Telefone: " << telefone << endl
 		 << "  Endereço: " << endereco << endl
 		 << "  Cidade: " << CIDADE << endl
 		 << "  Estado: " << UF << endl
 		 << "  CEP: " << cep << endl;
-
 }
+
+Cliente::Cliente(string n, string en, string c, string est, string cep, string tel): _nome(n), _endereco(en), _cidade(c), _estado(est), _cep(cep), _telefone(tel){}
