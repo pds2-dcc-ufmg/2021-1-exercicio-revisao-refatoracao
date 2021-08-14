328f837a4bdb0edccc5a86877e72e2f971bd8792
Mateusmsc3 <86414375+Mateusmsc3@users.noreply.github.com>
2021-08-14 18:26:44 -0300

    declaração do metodo print()

--- Cobertura.hpp
+++ Cobertura.hpp
@@ -11,18 +11,7 @@ using std::endl;
 class Cobertura : public Imovel {
     
     public:
-    void print() {
-
-        std::cout << "[Cobertura]" << endl;
-        Imovel::print();
-        std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
-                << "Taxa de Comissão: " << 10 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
-                << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
-    }
+        void print();
 };
 
 #endif
