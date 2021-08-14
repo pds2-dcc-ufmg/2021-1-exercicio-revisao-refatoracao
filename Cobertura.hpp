52d10f0b510b79aa6738fb5377692fdb9a55fb99
Artur <arthur.gaspar2011@gmail.com>
2021-08-14 15:00:31 -0300

    Adicionado Cobertura.cpp

--- Cobertura.hpp
+++ Cobertura.hpp
@@ -18,44 +18,13 @@ class Cobertura : public Imovel {
             double valor_m2,
             Cliente vendedor,
             string corretor
-        ):Imovel(
-            area,
-            quartos,
-            banheiros,
-            vagas,
-            valor_m2,
-            vendedor,
-            corretor
-        ){}
+        );
 
-        double valor() override {
+        double valor();
 
-            double val_total = area * valor_m2;
+        double comissao();
 
-            return val_total;
-
-        }
-
-        double comissao() override {
-
-            double comissao_cobertura = area * valor_m2;
-
-            return comissao_cobertura * taxa_comissao;
-            
-        }
-
-        void print() {
-
-            std::cout << "[Cobertura]" << endl;
-            Imovel::print();
-            std::cout << "Area: " << area << endl
-                    << "  Quartos: " << quartos << endl
-                    << "  Banheiros: " << banheiros << endl
-                    << "  Vagas: " << vagas << endl
-                    << "Taxa de Comissão: " << taxa_comissao*100 << "%" << endl
-                    << "Valor Comissão: R$ " << fixed << setprecision(2) << comissao_imovel << endl
-                    << "Valor de Venda: R$ " << fixed << setprecision(2) << valor_total << endl;
-        }
+        void print();
 };
 
 #endif
