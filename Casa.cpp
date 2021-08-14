35434196ca4056f6846d42e784fefeaa0ca3d0ef
Mateusmsc3 <86414375+Mateusmsc3@users.noreply.github.com>
2021-08-14 18:57:04 -0300

    retirada de números mágicos e comentários

--- Casa.cpp
+++ Casa.cpp
@@ -3,9 +3,12 @@
 #include "Cliente.hpp"
 #include "Casa.hpp"
 
+#define PORCENT_COMISSAO 0.06
+#define TAXA_COMISSAO 6
+
 Casa::Casa(string cor, double a, int q, int b, int vag, double valm2,  Cliente ven): Imovel(cor, a, q, b, vag, valm2, ven){
-    _porcComissao = 0.06;
-    _taxa = 6;
+    _porcComissao = PORCENT_COMISSAO;//variavel que armazena a porcentagem da comissao para casass
+    _taxa = TAXA_COMISSAO;// variavel que armazena a taxa de comissao para casas
 };
 
 void Casa::print() {
