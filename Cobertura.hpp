23dc12b16a357ae03953a1191158de9d9d34caff
becaroline <88947044+becaroline@users.noreply.github.com>
2021-08-15 16:15:00 -0300

    Update Cobertura.hpp

--- Cobertura.hpp
+++ Cobertura.hpp
@@ -11,7 +11,7 @@ class Cobertura : public Imovel {
 
     double valor() {
 
-        double v = AREA * VALORm2;
+        double v = area * valor_m2;
 
         return v;
 
@@ -19,7 +19,7 @@ class Cobertura : public Imovel {
 
     double comissao() {
 
-        double c = AREA * VALORm2;
+        double c = area * valor_m2;
 
         return c * 0.10;
         
@@ -29,13 +29,13 @@ class Cobertura : public Imovel {
 
         std::cout << "[Cobertura]" << endl;
         Imovel::print();
-        std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
+        std::cout << "Area: " << area << endl
+                << "  Quartos: " << qt_quartos << endl
+                << "  Banheiros: " << qt_banheiros << endl
+                << "  Vagas: " << qt_vagas << endl
                 << "Taxa de Comissão: " << 10 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
-                << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
+                << "Valor Comissão: R$ " << fixed << setprecision(2) << valor_comissao << endl
+                << "Valor de Venda: R$ " << fixed << setprecision(2) << valor_venda << endl;
     }
 };
 
