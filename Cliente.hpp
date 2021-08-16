469dccc8ae6f9a643a9ee1c198814f9edba268bf
GustavoHso7 <89020267+GustavoHso7@users.noreply.github.com>
2021-08-16 12:33:50 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -7,17 +7,21 @@ using namespace std;
 class Cliente {
     public:
         string NOME;
-        string endereco;
-        string CIDADE;
-        string UF;
-        string cep;
-        string telefone;
-
         string getNome();
+    
+        string endereco;
         string getEndereco();
+        
+        string CIDADE;
         string getCidade();
+        
+        string UF;
         string getUf();
+        
+        string cep; 
         string getCep();
+        
+        string telefone;
         string getTelefone();
         
         void print();
