9b256165c10759ea30ab5d9c40a84f1b04625403
becaroline <88947044+becaroline@users.noreply.github.com>
2021-08-16 12:34:25 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -12,3 +12,53 @@ void Cliente::print(){
 		 << "  CEP: " << cep << endl;
 
 }
+
+void Cliente::setNome(string _nome){
+       nome = _nome;
+}
+
+string Cliente::getNome(){
+       return nome;
+}
+
+void Cliente::setEndereço(string _endereço){
+       endereço = _endereço;
+}
+
+string Cliente::getEndereço(){
+       return endereço;
+}
+
+void Cliente::setCidade(string _cidade){
+       cidade = _cidade;
+}
+
+string Cliente::getCidade(){
+       return cidade;
+}
+
+void Cliente::setUf(string _uf){
+	uf = _uf;
+}
+
+string Cilente::getUf(){
+	return uf;
+}
+
+void Cliente::setCep(string _cep){
+       cep = _cep;
+}
+
+string Cliente::setCep(){
+       return cep;
+}
+
+void Cliente::setTelefone(string _telefone){
+       telefone = _telefone;
+}
+
+string Cliente::getTelefone(){
+        return telefone;
+}
+
+
