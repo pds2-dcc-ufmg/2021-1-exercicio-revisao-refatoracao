3f8cf6ee93599761457754ba21ab32c97ca65bef
becaroline <88947044+becaroline@users.noreply.github.com>
2021-08-16 13:33:11 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -7,7 +7,7 @@ using namespace std;
 class Cliente {
     public:
         // Criação de métodos get e set para acessar as variáveis
-        void setNome(string nome), setEndereço(string endereço), setCidade(string cidade), setUf(string uf), setCep(string cep), setTelefone(string telefone);
+        void setNome(string Nome), setEndereço(string Endereço), setCidade(string Cidade), setUf(string Uf), setCep(string Cep), setTelefone(string Telefone);
         string getNome(), getEndereço(), getCidade(), getUf(), getCep(), getTelefone();
 
         void print();
