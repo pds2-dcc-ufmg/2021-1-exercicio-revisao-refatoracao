ace6c6e28f5f6b9add98a54f23855de4d19d9762
Samuel Brisio <samuelbrisio@outlook.com>
2021-08-16 10:13:09 -0300

    Rename Atributes

--- Imovel.hpp
+++ Imovel.hpp
@@ -7,25 +7,25 @@
 
 class Imovel {
     protected:
-        double AREA;
-        int Q;
-        int B;
-        int V;
-        double VALORm2;
-        double Valor;
-        double C;
+        double area;
+        int n_quartos;
+        int n_banheiros;
+        int n_vagas;
+        double preco_por_m2;
+        double preco;
+        double comissao;
         Cliente vendedor;
         std::string corretor;
 
     public:
-        Imovel(double area, int q, int b, int v, double valor_m2, double valor, double c, Cliente vendedor, std::string corretor) {
-            AREA = area;
-            Q = q;
-            B = b;
-            V = v;
-            VALORm2 = valor_m2;
-            Valor = valor;
-            C = c;
+        Imovel(double area, int q, int b, int v, double valor_m2, double price, double c, Cliente vendedor, std::string corretor) {
+            this->area = area;
+            n_quartos = q;
+            n_banheiros = b;
+            n_vagas = v;
+            preco_por_m2 = valor_m2;
+            preco = price;
+            comissao = c;
             this->vendedor = vendedor;
             this->corretor = corretor;
         }
