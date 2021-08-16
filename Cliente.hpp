7c99d86ba286cc37fd9406aaaf6397e6285d2083
Pedro Giacomin <plsg2020.ufmg@gmail.com>
2021-08-16 18:53:15 -0300

    Construtor para Cliente
    Consegui tornar os atributos de Cliente private e limpar um pouco o main

--- Cliente.hpp
+++ Cliente.hpp
@@ -5,13 +5,27 @@
 using namespace std;
 
 class Cliente {
-    public:
+    
+    private:
         string NOME;
         string endereco;
         string CIDADE;
         string UF;
         string cep;
         string telefone;
+    
+    public:
+        //CONSTRUCTORS 
+        Cliente(){};
+
+        Cliente(string nome, string end, string cid, string uf, string cep, string tel){
+            this->NOME = nome;
+            this->endereco = end;
+            this->CIDADE = cid;
+            this->UF = uf;
+            this->cep = cep;
+            this->telefone = tel;
+        }
 
         void print();
 };
