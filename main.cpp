fe21718c11b3678cbb2eccb2bbb053c6853bda33
becaroline <88947044+becaroline@users.noreply.github.com>
2021-08-16 13:23:46 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,181 +9,187 @@
 
 int main() {
     
+    // Relação de clientes com imóveis para venda
     Cliente cl1;
-    cl1.nome = "Xayso Sovon Ziahaka";
-    cl1.endereco = "Rua Xangrilá - Braúnas";
-    cl1.cidade = "Belo Horizonte";
-    cl1.uf = "MG";
-    cl1.cep = "31365-570";
-    cl1.telefone = "3196007958";
+    cl1.setNome("Xayso Sovon Ziahaka");
+    cl1.setEndereco("Rua Xangrilá - Braúnas");
+    cl1.setCidade("Belo Horizonte");
+    cl1.setUf("MG");
+    cl1.setCep("31365-570");
+    cl1.setTelefone("3196007958");
     
     Cliente cl2;
-    cl2.nome = "Minia Pasies Kituos";
-    cl2.endereco = "Rua dos Jacobinos - Ouro Minas";
-    cl2.cidade = "Belo Horizonte";
-    cl2.uf = "MG";
-    cl2.cep = "31870-290";
-    cl2.telefone = "3197627067";
+    cl2.setNome("Minia Pasies Kituos");
+    cl2.setEndereco("Rua dos Jacobinos - Ouro Minas");
+    cl2.setCidade("Belo Horizonte");
+    cl2.setUf("MG");
+    cl2.setCep("31870-290");
+    cl2.setTelefone("3197627067");
     
     Cliente cl3;
-    cl3.nome = "Vuocue Leiur Baonauza";
-    cl3.endereco = "Rua Orminda de Almeida - Tupi B";
-    cl3.cidade = "Belo Horizonte";
-    cl3.uf = "MG";
-    cl3.cep = "31842-630";
-    cl3.telefone = "3195949327";
+    cl3.setNome("Vuocue Leiur Baonauza");
+    cl3.setEndereco("Rua Orminda de Almeida - Tupi B");
+    cl3.setCidade("Belo Horizonte");
+    cl3.setUf("MG");
+    cl3.setCep("31842-630");
+    cl3.setTelefone("3195949327");
     
     Cliente cl4;
-    cl4.nome = "Zerer Huduy Fyogar";
-    cl4.endereco = "Rua Taquaril - Jonas Veiga";
-    cl4.cidade = "Belo Horizonte";
-    cl4.uf = "MG";
-    cl4.cep = "30285-422";
-    cl4.telefone = "3198596327";
+    cl4.setNome("Zerer Huduy Fyogar");
+    cl4.setEndereco("Rua Taquaril - Jonas Veiga");
+    cl4.setCidade("Belo Horizonte");
+    cl4.setUf("MG");
+    cl4.setCep("30285-422");
+    cl4.setTelefone("3198596327");
     
     Cliente cl5;
-    cl5.nome = "Ceziel Mioti Pler";
-    cl5.endereco = "Rua João Gualberto Costa - Serrano";
-    cl5.cidade = "Belo Horizonte";
-    cl5.uf = "MG";
-    cl5.cep = "30882-747";
-    cl5.telefone = "3196274465";
+    cl5.setNome("Ceziel Mioti Pler");
+    cl5.setEndereco("Rua João Gualberto Costa - Serrano");
+    cl5.setCidade("Belo Horizonte");
+    cl5.setUf("MG");
+    cl5.setCep("30882-747");
+    cl5.setTelefone("3196274465");
     
     Cliente cl6;
-    cl6.nome = "Esxo Cilal Zyais";
-    cl6.endereco = "Rua Américo Luiz Moreira - Jardim dos Comerciários (Venda Nova)";
-    cl6.cidade = "Belo Horizonte";
-    cl6.uf = "MG";
-    cl6.cep = "31650-560";
-    cl6.telefone = "3195004414";
+    cl6.setNome("Esxo Cilal Zyais");
+    cl6.setEndereco("Rua Américo Luiz Moreira - Jardim dos Comerciários (Venda Nova)");
+    cl6.setCidade("Belo Horizonte");
+    cl6.setUf("MG");
+    cl6.setCep("31650-560");
+    cl6.setTelefone("3195004414");
     
     Cliente cl7;
-    cl7.nome = "Leova Wikyecil Neaca";
-    cl7.endereco = "Rua João Arantes - Cidade Nova";
-    cl7.cidade = "Belo Horizonte";
-    cl7.uf = "MG";
-    cl7.cep = "31170-240";
-    cl7.telefone = "3198461192";
+    cl7.setNome("Leova Wikyecil Neaca");
+    cl7.setEndereco("Rua João Arantes - Cidade Nova");
+    cl7.setCidade("Belo Horizonte");
+    cl7.setUf("MG");
+    cl7.setCep("31170-240");
+    cl7.setTelefone("3198461192");
     
     Cliente cl8;
-    cl8.nome = "Teas Heimeu Pipe";
-    cl8.endereco = "Rua Maria Pereira Damasceno - Ernesto do Nascimento(Barreiro)";
-    cl8.cidade = "Belo Horizonte";
-    cl8.uf = "MG";
-    cl8.cep = "30668-430";
-    cl8.telefone = "3197317802";
+    cl8.setNome("Teas Heimeu Pipe");
+    cl8.setEndereco("Rua Maria Pereira Damasceno - Ernesto do Nascimento(Barreiro)");
+    cl8.setCidade("Belo Horizonte");
+    cl8.setUf("MG");
+    cl8.setCep("30668-430");
+    cl8.setTelefone("3197317802");
 
     int qt_apartamento = 0;
     int qt_casa = 0;
     int qt_cobertura = 0;
 
+    //Relação de apartamentos para venda
     Apartamento ap1;
-    ap1.corretor = "Tuoruars";
-    ap1.area = 55.4;
-    ap1.qt_quartos = 2;
-    ap1.qt_banheiros = 1;
-    ap1.qt_vagas = 0;
-    ap1.valor_m2 = 987.0;
-    ap1.valor_comissao = ap1.comissao();
-    ap1.valor_venda = ap1.valor() + ap1.comissao();
-    ap1.vendedor = cl1;
+    ap1.setCorretor("Tuoruars");
+    ap1.setArea(55.4);
+    ap1.setQt_quartos(2);
+    ap1.setQt_banheiros(1);
+    ap1.setQt_vagas(0);
+    ap1.setValor_m2(987.0);
+    ap1.setValor_comissao(ap1.comissao());
+    ap1.setValor_venda(ap1.valor() + ap1.comissao());
+    ap1.setVendedor(cl1);
     qt_apartamento++;
 
     Apartamento ap2;
-    ap2.corretor = "Fyubyeis";
-    ap2.area = 74.5;
-    ap2.qt_quartos = 2;
-    ap2.qt_banheiros = 1;
-    ap2.qt_vagas = 2;
-    ap2.valor_m2 = 1540.0;
-    ap2.valor_comissao = ap2.comissao();
-    ap2.valor_venda = ap2.valor() + ap2.comissao();
-    ap2.vendedor = cl2;
+    ap2.setCorretor("Fyubyeis");
+    ap2.setArea(74.5);
+    ap2.setQt_quartos(2);
+    ap2.setQt_banheiros(1);
+    ap2.setQt_vagas(2);
+    ap2.setValor_m2(1540.0);
+    ap2.setValor_comissao(ap2.comissao());
+    ap2.setValor_venda(ap2.valor() + ap2.comissao());
+    ap2.setVendedor(cl2);
     qt_apartamento++;
 
     Apartamento ap3;
-    ap3.corretor = "Kelia";
-    ap3.area = 87.2;
-    ap3.qt_quartos = 3;
-    ap3.qt_banheiros = 2;
-    ap3.qt_vagas = 2;
-    ap3.valor_m2 = 2354.0;
-    ap3.valor_comissao = ap3.comissao();
-    ap3.valor_venda = ap3.valor() + ap3.comissao();
-    ap3.vendedor = cl3;
+    ap3.setCorretor("Kelia");
+    ap3.setArea(87.2);
+    ap3.setQt_quartos(3);
+    ap3.setQt_banheiros(2);
+    ap3.setQt_vagas(2);
+    ap3.setValor_m2(2354.0);
+    ap3.setValor_comissao(ap3.comissao());
+    ap3.setValor_venda(ap3.valor() + ap3.comissao());
+    ap3.setVendedor(cl3);
     qt_apartamento++;
 
-    // Cobertura
+    // Relação de coberturas para venda
     Cobertura cb1;
-    cb1.corretor = "Koci";
-    cb1.area = 120.1;
-    cb1.qt_quartos = 3;
-    cb1.qt_banheiros = 3;
-    cb1.qt_vagas = 2;
-    cb1.valor_m2 = 3123.5;
-    cb1.valor_comissao = cb1.comissao();
-    cb1.valor_venda = cb1.valor() + cb1.comissao();
-    cb1.vendedor = cl4;
+    cb1.setCorretor("Koci");
+    cb1.setArea(120.1);
+    cb1.setQt_quartos(3);
+    cb1.setQt_banheiros(3);
+    cb1.setQt_vagas(2);
+    cb1.setValor_m2(3123.5);
+    cb1.setValor_comissao(cb1.comissao());
+    cb1.setValor_venda(cb1.valor() + cb1.comissao());
+    cb1.setVendedor(cl4);
     qt_cobertura++;
     
     Cobertura cb2;
-    cb2.corretor = "Wail";
-    cb2.area = 134.8;
-    cb2.qt_quartos = 4;
-    cb2.qt_banheiros = 3;
-    cb2.qt_vagas = 3;
-    cb2.valor_m2 = 3578.2;
-    cb2.valor_comissao = cb2.comissao();
-    cb2.valor_venda = cb2.valor() + cb2.comissao();
-    cb2.vendedor = cl5;
+    cb2.setCorretor("Wail");
+    cb2.setArea(134.8);
+    cb2.setQt_quartos(4);
+    cb2.setQt_banheiros(3);
+    cb2.setQt_vagas(3);
+    cb2.setValor_m2(3578.2);
+    cb2.setValor_comissao(cb2.comissao());
+    cb2.setValor_venda(cb2.valor() + cb2.comissao());
+    cb2.setVendedor(cl5);
     qt_cobertura++;
 
     Cobertura cb3;
-    cb3.corretor = "Fival";
-    cb3.area = 180.0;
-    cb3.qt_vagas = 4;
-    cb3.qt_banheiros = 4;
-    cb3.qt_vagas = 4;
-    cb3.valor_m2 = 4165.7;
-    cb3.valor_comissao = cb3.comissao();
-    cb3.valor_venda = cb3.valor() + cb3.comissao();
-    cb3.vendedor = cl6;
+    cb3.setCorretor("Fival");
+    cb3.setArea(180.0);
+    cb3.setQt_vagas(4);
+    cb3.setQt_banheiros(4);
+    cb3.setQt_vagas(4);
+    cb3.setValor_m2(4165.7);
+    cb3.setValor_comissao(cb3.comissao());
+    cb3.setValor_venda(cb3.valor() + cb3.comissao());
+    cb3.setVendedor(cl6);
     qt_cobertura++;
 
-    // Casas
+    // Relação de casas para venda
     Casa ca1;
-    ca1.corretor = "Beydo";
-    ca1.area = 145.6;
-    ca1.qt_quartos = 3;
-    ca1.qt_banheiros = 3;
-    ca1.qt_vagas = 2;
-    ca1.valor_m2 = 4023.6;
-    ca1.valor_comissao = ca1.comissao();
-    ca1.valor_venda = ca1.valor() + ca1.comissao();
-    ca1.vendedor = cl7;
+    ca1.setCorretor("Beydo");
+    ca1.setArea(145.6);
+    ca1.setQt_quartos(3);
+    ca1.setQt_banheiros(3);
+    ca1.setQt_vagas(2);
+    ca1.setValor_m2(4023.6);
+    ca1.setValor_comissao(ca1.comissao());
+    ca1.setValor_venda(ca1.valor() + ca1.comissao());
+    ca1.setVendedor(cl7);
     qt_cobertura++;
 
     Casa ca2;
-    ca2.corretor = "Riuzi";
-    ca2.area = 245.0;
-    ca2.qt_quartos = 5;
-    ca2.qt_banheiros = 4;
-    ca2.qt_vagas = 4;
-    ca2.valor_m2 = 4856.2;
-    ca2.valor_comissao = ca2.comissao();
-    ca2.valor_venda = ca2.valor() + ca2.comissao();
-    ca2.vendedor = cl8;
+    ca2.setCorretor("Riuzi");
+    ca2.setArea(245.0);
+    ca2.setQt_quartos(5);
+    ca2.setQt_banheiros(4);
+    ca2.setQt_vagas(4);
+    ca2.setValor_m2(4856.2);
+    ca2.setValor_comissao(ca2.comissao());
+    ca2.setValor_venda(ca2.valor() + ca2.comissao());
+    ca2.setVendedor(cl8);
     qt_casa++;
 
     std::cout << "\n>> Relatório de Imóveis <<" << endl;
 
+    // Soma total dos valores de venda de cada imóvel
     double valor_apartamento = 0.0;
     double valor_cobertura= 0.0;
     double valor_casa = 0.0;
+    
+    //Soma total dos valores da comissão de cada imóvel
     double comiss_apartamento = 0.0;
     double comiss_cobertura = 0.0;
     double comiss_casa = 0.0;
     
+    // Escrita das informações de cada imóvel e o valor total de venda e comissão
     ap1.print();
     valor_apartamento += ap1.valor();
     comiss_apartamento += ap1.comissao();
@@ -224,6 +230,7 @@ int main() {
     comiss_casa += ca2.comissao();
     std::cout << endl;
 
+    // Escrita do resumo final de vendas
     std::cout << "\n>> Resumo Geral <<" << endl;
 
     std::cout << "\n>>Apartamentos<<\n"
@@ -241,4 +248,6 @@ int main() {
             << "\n Valor Total: R$ " << fixed << setprecision(2) << valor_casa
             << "\n Comissão Total: R$ " << fixed << setprecision(2) << comiss_casa
             << endl;
+    
+    // Fim do código
 }
