aa9ae6b0915e31e7058bd50eba36f5c149b901bc
Bilacomy <87915087+Bilacomy@users.noreply.github.com>
2021-08-17 12:08:32 -0300

    Update Cobertura.hpp

--- Cobertura.hpp
+++ Cobertura.hpp
@@ -34,7 +34,7 @@ class Cobertura : public Imovel {
                 << "  Banheiros: " << banheiros << endl
                 << "  Vagas: " << vagas << endl
                 << "Taxa de Comissão: " << 10 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << comissao << endl
+                << "Valor Comissão: R$ " << fixed << setprecision(2) << comissao << endl;
                 << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
     }
 };
