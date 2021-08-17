546e2c19090dd9af25154939a2262911ea7258b7
Ana Luiza <analuizamacedost@gmail.com>
2021-08-17 14:12:22 -0300

    Alteração 01 Apartamento.hpp
    eliminei o std, uma vez que está sendo utilizado o using namespace, reajustei o nome das variáveis, como comissaoCorretor, visando explicar o numero magico 0,04

--- Apartamento.hpp
+++ Apartamento.hpp
@@ -12,30 +12,30 @@ class Apartamento : public Imovel {
     
     double valor() {
 
-        double v = AREA * VALORm2;
+        double valorApartamento = area * valorM2;
 
-        return v;
+        return valorApartamento;
 
     }
 
     double comissao() {
 
-        double c = AREA * VALORm2;
+        double comissaoCorretor = area * valorM2;
 
-        return c * 0.04;
+        return comissaoCorretor * 0.04; 
         
     }
 
     void print() {
 
-        std::cout << "[Apartamento]" << endl;
+        cout << "[Apartamento]" << endl;
         Imovel::print();
-        std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
+        cout << "Area: " << area << endl
+                << "  Quartos: " << quartos << endl
+                << "  Banheiros: " << banheiros << endl
+                << "  Vagas: " << vagas << endl
                 << "Taxa de Comissão: " << 4 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
+                << "Valor Comissão: R$ " << fixed << setprecision(2) << Comissao << endl
                 << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
     }
 };
