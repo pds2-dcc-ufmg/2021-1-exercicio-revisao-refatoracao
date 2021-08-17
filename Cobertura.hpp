9ffff9dc21e3f13f3adb7c4aea15c366778f04a5
lgmufmg <86167093+lgmufmg@users.noreply.github.com>
2021-08-17 15:53:33 -0300

    Update Cobertura.hpp
    - Alteração dos nomes dos atributos de Imovel para melhor compreensão do código.

--- Cobertura.hpp
+++ Cobertura.hpp
@@ -24,11 +24,11 @@ class Cobertura : public Imovel {
         std::cout << "[Cobertura]" << endl;
         Imovel::print();
         std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
+                << "  Quartos: " << NUM_QUARTOS << endl
+                << "  Banheiros: " << NUM_BANHEIROS << endl
+                << "  Vagas: " << NUM_VAGAS << endl
                 << "Taxa de Comissão: " << 10 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
+                << "Valor Comissão: R$ " << fixed << setprecision(2) << valorComissao << endl
                 << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
     }
 };
