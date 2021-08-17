8ef69bd942bb25ed0071e4177b9a8d64a6f9d217
GustavoHso7 <89020267+GustavoHso7@users.noreply.github.com>
2021-08-17 08:27:36 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -2,32 +2,32 @@
 #include <string>
 #include "Cliente.hpp"
 
- string Cliente::getNome() {
-	 return "  Nome: " + this->NOME;
+ string Cliente::get_nome() {
+	 return "  Nome: " + this->nome;
  }
- string Cliente::getEndereco() {
+ string Cliente::get_endereco() {
 	 return "  Endereço: " + this->endereco;
  }
- string Cliente::getCidade() {
-	 return "  Cidade: " + this->CIDADE;
+ string Cliente::get_cidade() {
+	 return "  Cidade: " + this->cidade;
  }
- string Cliente::getUf() {
-	 return "  Estado: " + this->UF;
+ string Cliente::get_uf() {
+	 return "  Estado: " + this->uf;
  }
- string Cliente::getCep() {
+ string Cliente::get_cep() {
 	 return "  CEP: " + this->cep;
  }
- string Cliente::getTelefone() {
+ string Cliente::get_telefone() {
 	 return "  Telefone: " + this->telefone;
  }
 
 void Cliente::print(){
 
-	std::cout << this->getNome() << endl
-		 << this->getTelefone() << endl
-		 << this->getEndereco() << endl
-		 << this->getCidade() << endl
-		 << this->getUf() << endl
-		 << this->getCep() << endl;
+	std::cout << this->get_nome() << endl
+		 << this->get_telefone() << endl
+		 << this->get_endereco() << endl
+		 << this->get_cidade() << endl
+		 << this->get_uf() << endl
+		 << this->get_cep() << endl;
 
 }
