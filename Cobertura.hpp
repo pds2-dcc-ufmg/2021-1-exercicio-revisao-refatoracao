a9b78e0c207527d88faf2e6094d1f79ce7a00bc9
Bilacomy <87915087+Bilacomy@users.noreply.github.com>
2021-08-17 15:02:32 -0300

    Update Cobertura.hpp

--- Cobertura.hpp
+++ Cobertura.hpp
@@ -8,25 +8,8 @@ using namespace std;
 class Cobertura : public Imovel {
     
     public:
-
-    double comissao() {
-
-        double comissao = area * valor_m2;
-        return comissao * 0.10;
-    }
-
-    void print() {
-
-        std::cout << "[Cobertura]" << endl;
-        Imovel::print();
-        std::cout << "Area: " << area << endl
-                << "  Quartos: " << quartos << endl
-                << "  Banheiros: " << banheiros << endl
-                << "  Vagas: " << vagas << endl
-                << "Taxa de Comissão: " << 10 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << comissao << endl
-                << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
-    }
+    double comissao();
+    void print();
 };
 
 #endif
