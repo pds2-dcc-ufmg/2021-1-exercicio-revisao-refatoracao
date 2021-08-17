9fe380f5ae23eba7a25d2471f2d9cf807ef9d978
IgorSartori <71766420+IgorSartori@users.noreply.github.com>
2021-08-17 18:07:53 -0300

    Update Imovel.cpp
    - Implementei o construtor para a classe "Imovel"

--- Imovel.cpp
+++ Imovel.cpp
@@ -4,6 +4,16 @@
 
 using namespace std;
 
+Imovel::Imovel(string _corretor, double _AREA, int _Q, int _B, int _V, double _VALORm2, Cliente _vendedor){
+  corretor = _corretor, 
+  AREA = _AREA, 
+  Q = _Q, 
+  B = _B, 
+  V = _V, 
+  VALORm2 = _VALORm2, 
+  vendedor = _vendedor
+}
+
 void Imovel::print(){
 
   cout << "[Vendedor]" << endl;
