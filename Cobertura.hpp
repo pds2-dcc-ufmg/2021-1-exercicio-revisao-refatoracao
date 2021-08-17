81beea4f5c8b8b7aa23a5b2d036a6f64cc3fb463
Pedro Faluba <pedrofaluba@ufmg.br>
2021-08-17 18:28:29 -0300

    Alterações feitas

--- Cobertura.hpp
+++ Cobertura.hpp
@@ -3,40 +3,21 @@
 
 #include "Imovel.hpp"
 #include "Cliente.hpp"
-using namespace std;
 
 class Cobertura : public Imovel {
+
+    protected:
+
+    double tax=0.1;
+    int print_tax=10;
     
     public:
 
-    double valor() {
+    double valor();
 
-        double v = AREA * VALORm2;
+    double comissao();
 
-        return v;
-
-    }
-
-    double comissao() {
-
-        double c = AREA * VALORm2;
-
-        return c * 0.10;
-        
-    }
-
-    void print() {
-
-        std::cout << "[Cobertura]" << endl;
-        Imovel::print();
-        std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
-                << "Taxa de Comissão: " << 10 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
-                << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
-    }
+    void print();
 };
 
 #endif
--- Imovel.hpp
+++ Imovel.hpp
@@ -5,26 +5,53 @@
 #include <iomanip>
 #include "Cliente.hpp"
 
-using namespace std;
-
 class Imovel {
     public:
-        double AREA;
-        int Q;
-        int B;
-        int V;
-        double VALORm2;
-        double Valor;
-        double C;
-        Cliente vendedor;
-        string corretor;
+        
+    double area;
+    int quarto;
+    int banheiro;
+    int vagas;
+    double valor_m2;
+    double valor;
+    double comissao;
+    Cliente vendedor;
+    string corretor;
 
-        void print() {
-            cout << "[Vendedor]" << endl;
-            vendedor.print();
-            cout << "[Corretor]" << endl;
-            cout << "  " + corretor << endl;
-        }
+    void print() 
+    {
+        cout << "[Vendedor]" << endl;
+        vendedor.print();
+        cout << "[Corretor]" << endl;
+        cout << "  " + corretor << endl;
+    }
+
+    double calc_valor()
+    {
+        double value = area * valor_m2;
+        return value;
+    }
+
+
+    double calc_comissao(double tax_)
+    {
+        double comission = area * valor_m2;
+        return comission*tax_;
+    }
+
+    void printer(int print_tax_)
+    {
+        std::cout << "[Apartamento]" << endl;
+        Imovel::print();
+        std::cout << "Area: " << area << endl
+        << "  Quartos: " << quarto << endl
+        << "  Banheiros: " << banheiro << endl
+        << "  Vagas: " << vagas << endl
+        << "Taxa de Comissão: " << print_tax_ << "%" << endl
+        << "Valor Comissão: R$ " << fixed << setprecision(2) << comissao << endl
+        << "Valor de Venda: R$ " << fixed << setprecision(2) << valor << endl;
+    }
+    
 };
 
 #endif
