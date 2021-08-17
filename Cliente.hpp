2cdf92f16e753e173a5e2f083e3f5cf69075ecee
ReiNicolas <88855197+ReiNicolas@users.noreply.github.com>
2021-08-17 10:48:07 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -6,12 +6,38 @@ using namespace std;
 
 class Cliente {
     public:
-        string NOME;
-        string endereco;
-        string CIDADE;
+        string Nome;
+        string Endereco;
+        string Cidade;
         string UF;
-        string cep;
-        string telefone;
+        string Cep;
+        string Telefone;
+    
+        Cliente(string _Nome, string _Endereco, string _Cidade, string _UF, string _Cep, string _Telefone);
+    
+        string GetNome();
+    
+        string GetEndereco();
+    
+        string GetCidade();
+    
+        string GetUF();
+    
+        string GetCep();
+    
+        string GetTelefone();
+    
+        void SetNome(string _Nome);
+    
+        void SetEndereco(string _Endereco);
+    
+        void SetCidade(string _Cidade);
+    
+        void SetUF(string _UF);
+    
+        void SetCep(string _Cep);
+    
+        void SetTelefone(string _Telefone);
 
         void print();
 };
