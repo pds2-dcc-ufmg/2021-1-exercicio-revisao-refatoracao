1bf8bccdbce10c7770d498ad355c413aee741e08
Ga-Alves <78097460+Ga-Alves@users.noreply.github.com>
2021-08-17 17:13:01 -0300

    encapsula cliente

--- include/Cliente.hpp
+++ include/Cliente.hpp
@@ -4,14 +4,14 @@
 #include <string>
 
 class Cliente {
-    public:
+    private:
         std::string NOME;
         std::string endereco;
         std::string CIDADE;
         std::string UF;
         std::string cep;
         std::string telefone;
-
+    public:
         Cliente();
         Cliente(std::string Nome, std::string Endereco, std::string cidade, std::string Uf, std::string cep, std::string telefone);
         void print();
