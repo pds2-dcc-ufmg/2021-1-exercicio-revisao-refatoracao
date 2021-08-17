b3cb610fdbb165f17f1c1d58b916c7a8eb68cf3e
misscodes <>
2021-08-17 19:49:47 +0000

    refatoracao feita

--- Apartamento.hpp
+++ Apartamento.hpp
@@ -10,20 +10,24 @@ class Apartamento : public Imovel {
 
     public: 
     
-    double valor() {
+    taxa_corretagem = 0.04;
 
-        double v = AREA * VALORm2;
+    double valor() override {
 
-        return v;
+        return AREA * VALORm2;
 
     }
 
-    double comissao() {
+    double comissao() override {
 
-        double c = AREA * VALORm2;
-
-        return c * 0.04;
+        return this->valor * taxa_corretagem;
         
+    }
+
+    double ValorFinal() override {
+     
+      return this->valor + this->comissao;
+      
     }
 
     void print() {
@@ -31,11 +35,11 @@ class Apartamento : public Imovel {
         std::cout << "[Apartamento]" << endl;
         Imovel::print();
         std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
+                << "  Quartos: " << Quartos << endl
+                << "  Banheiros: " << Banheiros << endl
+                << "  Vagas: " << Vagas << endl
                 << "Taxa de Comissão: " << 4 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
+                << "Valor Comissão: R$ " << fixed << setprecision(2) << Comissao << endl
                 << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
     }
 };
--- Imovel.hpp
+++ Imovel.hpp
@@ -10,14 +10,18 @@ using namespace std;
 class Imovel {
     public:
         double AREA;
-        int Q;
-        int B;
-        int V;
+        int Quartos;
+        int Banheiros;
+        int Vagas;
         double VALORm2;
         double Valor;
-        double C;
+        double Comissao;
         Cliente vendedor;
         string corretor;
+        double taxa_corretagem;
+        virtual double valor();
+        virtual double comissao();
+        virtual double ValorFinal();
 
         void print() {
             cout << "[Vendedor]" << endl;
