20720488c0c01f703edb63f8307bfb732f76a896
jonhjonhb <augustosales2010@gmail.com>
2021-08-17 01:00:19 -0300

    Correcao de bugs, alteracao e otimizacao do codigo.

--- Apartamento.hpp
+++ Apartamento.hpp
@@ -7,37 +7,18 @@
 using namespace std;
 
 class Apartamento : public Imovel {
+    public:
+      Apartamento(){
+        TAXAcomissao = 0.04; 
+      }
+      
+      double valor() {return AREA * VALORm2;}
+      double comissao() {return valor() * TAXAcomissao;}
 
-    public: 
-    
-    double valor() {
-
-        double v = AREA * VALORm2;
-
-        return v;
-
-    }
-
-    double comissao() {
-
-        double c = AREA * VALORm2;
-
-        return c * 0.04;
-        
-    }
-
-    void print() {
-
+      void print() {
         std::cout << "[Apartamento]" << endl;
         Imovel::print();
-        std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
-                << "Taxa de Comissão: " << 4 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
-                << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
-    }
+      }
 };
 
 #endif
