7f93343a282ef4aee8f5fcc894501bb7cf3ecab1
IgorSartori <71766420+IgorSartori@users.noreply.github.com>
2021-08-17 20:18:39 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -4,25 +4,27 @@
 
 using namespace std;
 
-Cliente::(string _NOME, string _endereco, string _CIDADE, 
-          string _UF, string _cep, string _telefone){
+Cliente::Cliente(){}
 
-	NOME = _NOME;
-        endereco = _endereco;
-        CIDADE = _CIDADE;
-        UF = _UF;
-        cep = _cep;
-        telefone = _telefone;
+Cliente::Cliente(string _NOME, string _endereco, string _CIDADE, 
+          	 string _UF, string _cep, string _telefone){
+
+    NOME = _NOME;
+    endereco = _endereco;
+    CIDADE = _CIDADE;
+    UF = _UF;
+    cep = _cep;
+    telefone = _telefone;
 
 }
 
 void Cliente::print(){
 
-	cout << "  Nome: " << NOME << endl
-	cout << "  Telefone: " << telefone << endl
-	cout << "  Endereço: " << endereco << endl
-	cout << "  Cidade: " << CIDADE << endl
-	cout << "  Estado: " << UF << endl
+	cout << "  Nome: " << NOME << endl;
+	cout << "  Telefone: " << telefone << endl;
+	cout << "  Endereço: " << endereco << endl;
+	cout << "  Cidade: " << CIDADE << endl;
+	cout << "  Estado: " << UF << endl;
 	cout << "  CEP: " << cep << endl;
 
 }
