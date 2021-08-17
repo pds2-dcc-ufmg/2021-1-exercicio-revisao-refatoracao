264e91231d4f188d6b3d2b9ba55b004740c3d91f
LucasGontijoC <89094649+LucasGontijoC@users.noreply.github.com>
2021-08-17 16:53:48 -0300

    Update Imovel.hpp
    - Declaração do Valor_Imovel como resultado de AREA * VALORm2.
    - Declaração dos valores percentuais de taxa de corretagem.

--- Imovel.hpp
+++ Imovel.hpp
@@ -10,12 +10,16 @@ using namespace std;
 class Imovel {
     public:
         double AREA;
-        int Q;
-        int B;
-        int V;
+        int Qnt_Quartos;
+        int Qnt_Banheiros;
+        int Qnt_Vagas;
         double VALORm2;
-        double Valor;
-        double C;
+        double Valor_Venda;
+        double Valor_Imovel = AREA * VALORm2;
+        double Valor_Comissao;
+        double Perc_Comissao_Ap = 0.04;
+        double Perc_Comissao_Casa = 0.06;
+        double Perc_Comissao_Cobert = 0.10;
         Cliente vendedor;
         string corretor;
 
