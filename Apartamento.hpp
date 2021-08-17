e39d7c8705097d95fe2adb3d6367316c1e604e94
Bilacomy <87915087+Bilacomy@users.noreply.github.com>
2021-08-17 14:55:35 -0300

    Update Apartamento.hpp

--- Apartamento.hpp
+++ Apartamento.hpp
@@ -9,25 +9,8 @@ using namespace std;
 class Apartamento : public Imovel {
 
     public: 
-
-    double comissao() {
-
-        double comissao = area * valor_m2;
-        return comissao * 0.04;
-    }
-
-    void print() {
-
-        std::cout << "[Apartamento]" << endl;
-        Imovel::print();
-        std::cout << "Area: " << area << endl
-                << "  Quartos: " << quartos << endl
-                << "  Banheiros: " << banheiros << endl
-                << "  Vagas: " << vagas << endl
-                << "Taxa de Comissão: " << 4 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << comissao << endl
-                << "Valor de Venda: R$ " << fixed << setprecision(2) << valor << endl;
-    }
+    double comissao();
+    void print();
 };
 
 #endif
