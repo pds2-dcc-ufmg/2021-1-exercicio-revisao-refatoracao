87e69206726477a85609fb5801c5fb61e85ac1c8
ReiNicolas <88855197+ReiNicolas@users.noreply.github.com>
2021-08-17 10:17:23 -0300

    Update Apartamento.hpp

--- Apartamento.hpp
+++ Apartamento.hpp
@@ -23,18 +23,6 @@ class Apartamento : public Imovel {
     Apartamento(double _Area, int _Quartos, int _Banheiros, int _Vagas, double _VALORm2, string _Corretor, Cliente _Vendedor): Imovel(double _Area, int _Quartos, int _Banheiros, int _Vagas, double _VALORm2, string _Corretor, Cliente _Vendedor){
     };
 
-    void print() {
-
-        std::cout << "[Apartamento]" << endl;
-        Imovel::print();
-        std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
-                << "Taxa de Comissão: " << 4 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
-                << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
-    }
 };
 
 #endif
