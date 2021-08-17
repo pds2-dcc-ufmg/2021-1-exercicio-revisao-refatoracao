802d4a8cbaec4b22b01b4394490472e24ea66f9b
IgorSartori <71766420+IgorSartori@users.noreply.github.com>
2021-08-17 20:10:14 -0300

    Update Casa.hpp

--- Casa.hpp
+++ Casa.hpp
@@ -8,27 +8,34 @@ class Casa : public Imovel {
     
     public:    
     
-    taxa = 6;
+    Casa(string _corretor, double _AREA, int _Q, int _B, int _V, double _VALORm2, Cliente _vendedor){
+                                 
+        corretor = _corretor; 
+        AREA = _AREA; 
+        Q = _Q;
+        B = _B; 
+        V = _V;
+        VALORm2 = _VALORm2;
+        vendedor = _vendedor;
+        taxa = 6;
+    }
     
     double valor() {
 
         double v = AREA * VALORm2;
-
         return v;
-
     }
 
     double comissao() {
 
         return valor() * (taxa/100);
-        
     }
 
     void print() {
 
         std::cout << "[Casa]" << endl;
         Imovel::print();
-        Imovel::printImov(taxa);
+        Imovel::printImov();
     }
 };
 
