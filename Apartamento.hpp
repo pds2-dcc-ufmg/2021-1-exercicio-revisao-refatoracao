33d9c6db2ddc80ae6c49806c9f6dc69e901ea68c
lgmufmg <86167093+lgmufmg@users.noreply.github.com>
2021-08-17 15:47:56 -0300

    Update Apartamento.hpp
    - Atualização de nomes de variáveis chamadas.

--- Apartamento.hpp
+++ Apartamento.hpp
@@ -26,11 +26,11 @@ class Apartamento : public Imovel {
         std::cout << "[Apartamento]" << endl;
         Imovel::print();
         std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
+                << "  Quartos: " << NUM_QUARTOS << endl
+                << "  Banheiros: " << NUM_BANHEIROS << endl
+                << "  Vagas: " << NUM_VAGAS << endl
                 << "Taxa de Comissão: " << 4 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
+                << "Valor Comissão: R$ " << fixed << setprecision(2) << valorComissao << endl
                 << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
     }
 };
