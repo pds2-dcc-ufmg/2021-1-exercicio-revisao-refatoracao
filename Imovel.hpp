59a33ab26d40cb03992b85d5ad8885e946fd9f76
Eduardo Assis Tomich <29799589+eduardotomich@users.noreply.github.com>
2021-08-17 17:08:49 -0300

    Update Imovel.hpp
    Mudei o nome das variaveis "AREA, VALORm2, Q, B ,V, Valor, C", para "area, valor_m2, n_quartos, n_banheiros, n_vagas, valor_total, e comissao" respectivamente.

--- Imovel.hpp
+++ Imovel.hpp
@@ -9,13 +9,13 @@ using namespace std;
 
 class Imovel {
     public:
-        double AREA;
-        int Q;
-        int B;
-        int V;
-        double VALORm2;
-        double Valor;
-        double C;
+        double area;
+        int n_quartos;
+        int n_banheiros;
+        int n_vagas;
+        double valor_m2;
+        double valor_total;
+        double comissao;
         Cliente vendedor;
         string corretor;
 
