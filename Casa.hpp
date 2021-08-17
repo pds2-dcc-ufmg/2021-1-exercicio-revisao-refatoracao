f6eec586035e65316f034e7bb9ba9981acce1836
IgorSartori <71766420+IgorSartori@users.noreply.github.com>
2021-08-17 16:23:04 -0300

    Update Casa.hpp
    - criei uma variável chamada "taxa" para lidar com o numero magico
    - criei uma função no Imovel.hpp e Imovel.cpp para imprimir grande parte das informações

--- Casa.hpp
+++ Casa.hpp
@@ -4,12 +4,12 @@
 #include "Imovel.hpp"
 #include "Cliente.hpp"
 
-using namespace std;
-
 class Casa : public Imovel {
-
-    public:
-
+    
+    public:    
+    
+    taxa = 6;
+    
     double valor() {
 
         double v = AREA * VALORm2;
@@ -20,9 +20,7 @@ class Casa : public Imovel {
 
     double comissao() {
 
-        double c = AREA * VALORm2;
-
-        return c * 0.06;
+        return valor() * (taxa/100);
         
     }
 
@@ -30,15 +28,10 @@ class Casa : public Imovel {
 
         std::cout << "[Casa]" << endl;
         Imovel::print();
-        std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
-                << "Taxa de Comissão: " << 6 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
-                << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
+        Imovel::printImov(taxa);
 
     }
+    
 
 };
 
