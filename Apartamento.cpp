f8763dd2e1c2c2f60e25aa9ea3307ec8085ab1c1
Bilacomy <87915087+Bilacomy@users.noreply.github.com>
2021-08-17 14:53:39 -0300

    Update Apartamento.cpp

--- Apartamento.cpp
+++ Apartamento.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <string>
+#include "Apartamento.hpp"  
 
+double Apartamento::comissao(){
+
+    double comissao = area * valor_m2;
+        return comissao * 0.04;
+    }
+
+    void Apartamento::print(){
+
+        std::cout << "[Apartamento]" << endl;
+        Imovel::print();
+        std::cout << "Area: " << area << endl
+                << "  Quartos: " << quartos << endl
+                << "  Banheiros: " << banheiros << endl
+                << "  Vagas: " << vagas << endl
+                << "Taxa de Comissão: " << 4 << "%" << endl
+                << "Valor Comissão: R$ " << fixed << setprecision(2) << comissao << endl
+                << "Valor de Venda: R$ " << fixed << setprecision(2) << valor << endl;
+    }
+};
