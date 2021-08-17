b394c5159916b4d4e84b9db6e5d1b9475dd8cb35
ReiNicolas <88855197+ReiNicolas@users.noreply.github.com>
2021-08-17 10:17:21 -0300

    Update Imovel.hpp

--- Imovel.hpp
+++ Imovel.hpp
@@ -19,10 +19,19 @@ class Imovel {
 
     public:
         void print() {
+            std::cout << this.TipoDeImovel() << endl;
+            
             cout << "[Vendedor]" << endl;
             vendedor.print();
             cout << "[Corretor]" << endl;
             cout << "  " + corretor << endl;
+            std::cout << "Area: " << this.GetArea() << endl
+                << "  Quartos: " << this.GetQuartos() << endl
+                << "  Banheiros: " << this.GetBanheiros() << endl
+                << "  Vagas: " << this.GetVagas() << endl
+                << "Taxa de Comissão: " << 10 << "%" << endl
+                << "Valor Comissão: R$ " << fixed << setprecision(2) << this.GetComissao() << endl
+                << "Valor de Venda: R$ " << fixed << setprecision(2) << this.ValorDeVenda() << endl;
         }
     
         double ~GetPercentComissao()=0;
@@ -44,74 +53,75 @@ class Imovel {
             this.Vendedor=_Vendedor;
         }
     
-    double GetArea(){
-        return this.Area;
-    }
+        double GetArea(){
+            return this.Area;
+        }
     
-    void SetArea(double _Area){
-        this.Area=_Area;
-    }
+        void SetArea(double _Area){
+            this.Area=_Area;
+        }
     
-    int GetQuartos(){
-        return this.Quartos;
-    }
+        int GetQuartos(){
+            return this.Quartos;
+        }
     
-    void SetQuartos(int _Quartos){
-        this.Quartos=_Quartos;
-    }
+        void SetQuartos(int _Quartos){
+            this.Quartos=_Quartos;
+        }
     
-    int GetBanheiros(){
-        return this.Banheiros;
-    }
+        int GetBanheiros(){
+            return this.Banheiros;
+        }
     
-    void SetBanheiros(int _Banheiros){
-        this.Banheiros=_Banheiros;
-    }
+        void SetBanheiros(int _Banheiros){
+            this.Banheiros=_Banheiros;
+        }
     
-    int GetVagas(){
-        return this.Vagas;
-    }
+        int GetVagas(){
+            return this.Vagas;
+        }
     
-    void SetVagas(int _Vagas){
-        this.Vagas=_Vagas;
-    }
+        void SetVagas(int _Vagas){
+            this.Vagas=_Vagas;
+        }
     
-    double GetValoM2(){
-        return this.ValorM2;
-    }
+        double GetValoM2(){
+            return this.ValorM2;
+        }
     
-    void SetValorM2(double _ValorM2){
-        this.ValorM2=_ValorM2;
-    }
+        void SetValorM2(double _ValorM2){
+            this.ValorM2=_ValorM2;
+        }
     
-    double GetValor(){
-        return AREA*VALORm2;
-    }
+        double GetValor(){
+            return AREA*VALORm2;
+        }
     
-    double GetComissao(){
-        return this.valor()*GetPercentComissao();
-    }
+        double GetComissao(){
+            return this.valor()*GetPercentComissao();
+        }
     
-    double GetValorDeVenda(){
-        //O valor de venda e a soma da comissao e do valor do ímovel.
-        return this.comissao()+this.valor()
-    }
+        double GetValorDeVenda(){
+            //O valor de venda e a soma da comissao e do valor do ímovel.
+            return this.comissao()+this.valor()
+        }
         
     
-    Cliente GetVendedor(){
-        return this.Vendedor;
-    }
+        Cliente GetVendedor(){
+            return this.Vendedor;
+        }
     
-    void SetVendedor(Cliente _Vendedor){
-        this.Vendedor=_Vendedor;
+        void SetVendedor(Cliente _Vendedor){
+            this.Vendedor=_Vendedor;
+        }
     
-    string GetCorretor(){
-        return this.Corretor;
-    }
+        string GetCorretor(){
+            return this.Corretor;
+        }
         
-    void SetCorretor(string _Corretor){
-        this.Corretor=_Corretor;
-    }
+        void SetCorretor(string _Corretor){
+            this.Corretor=_Corretor;
+        }
     
 };
 
