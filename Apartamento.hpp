8644f060eb0c9120bd006acdb83ae7fed0d78c40
Eduardo Assis Tomich <29799589+eduardotomich@users.noreply.github.com>
2021-08-17 20:48:29 -0300

    Update Apartamento.hpp
    separação do .cpp para modularização/organização do código

--- Apartamento.hpp
+++ Apartamento.hpp
@@ -1,6 +1,5 @@
 #ifndef APARTAMENTO_HPP
 #define APARTAMENTO_HPP
-#define COMISSAOPERCENT 4
 
 #include "Imovel.hpp"
 #include "Cliente.hpp"
@@ -11,27 +10,10 @@ class Apartamento : public Imovel {
 
     public: 
     
-    double getValor() {
-        double valor = AREA * VALORm2;
-        return valor;
-    }
+        double getValor() {
+     
+        double getComissao();
 
-    double getComissao() {
-        return this->getValor * 0.04;
-    }
-
-    void print() {
-
-        std::cout << "[Apartamento]" << endl;
-        Imovel::print();
-        std::cout << "Area: " << area << endl
-                << "  Quartos: " << n_quartos << endl
-                << "  Banheiros: " << n_banheiros << endl
-                << "  Vagas: " << n_vagas << endl
-                << "Taxa de Comissão: " << COMISSAOPERCENT << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << comissao << endl
-                << "Valor de Venda: R$ " << fixed << setprecision(2) << valor_total << endl;
-    }
+        void print();
 };
-
 #endif
