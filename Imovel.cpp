d1e0c3ac5399fda36300f73d62f77a9f125fcd99
IgorSartori <71766420+IgorSartori@users.noreply.github.com>
2021-08-17 20:09:50 -0300

    Update Imovel.cpp

--- Imovel.cpp
+++ Imovel.cpp
@@ -4,16 +4,6 @@
 
 using namespace std;
 
-Imovel::Imovel(string _corretor, double _AREA, int _Q, int _B, int _V, double _VALORm2, Cliente _vendedor){
-  corretor = _corretor, 
-  AREA = _AREA, 
-  Q = _Q, 
-  B = _B, 
-  V = _V, 
-  VALORm2 = _VALORm2, 
-  vendedor = _vendedor
-}
-
 void Imovel::print(){
 
   cout << "[Vendedor]" << endl;
@@ -23,13 +13,13 @@ void Imovel::print(){
 
 }
 
-void Imovel::printImov(double t){
+void Imovel::printImov(){
 
   cout << "Area: " << AREA << endl;
   cout << "  Quartos: " << Q << endl;
   cout << "  Banheiros: " << B << endl;
   cout << "  Vagas: " << V << endl;
-  cout << "Taxa de Comissão: " << t << "%" << endl;
+  cout << "Taxa de Comissão: " << taxa << "%" << endl;
   cout << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl;
   cout << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
 
