0ae87084a877c5952e38ab10c3bbb7af9304f364
IgorSartori <71766420+IgorSartori@users.noreply.github.com>
2021-08-17 20:11:48 -0300

    Update Cobertura.hpp

--- Cobertura.hpp
+++ Cobertura.hpp
@@ -8,27 +8,34 @@ class Cobertura : public Imovel {
     
     public:    
     
-    taxa = 10;
-    
+    Cobertura(string _corretor, double _AREA, int _Q, int _B, int _V, double _VALORm2, Cliente _vendedor){
+                                 
+        corretor = _corretor; 
+        AREA = _AREA; 
+        Q = _Q;
+        B = _B; 
+        V = _V;
+        VALORm2 = _VALORm2;
+        vendedor = _vendedor;
+        taxa = 10;
+    }
+
     double valor() {
 
         double v = AREA * VALORm2;
-
         return v;
-
     }
 
     double comissao() {
-
-        return valor() * (taxa/100);
         
+        return valor() * (taxa/100);
     }
 
     void print() {
 
         std::cout << "[Cobertura]" << endl;
         Imovel::print();
-        Imovel::printImov(taxa);
+        Imovel::printImov();
     }
 };
 
