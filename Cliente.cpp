7acd6d6a4e8ffff485db89beebbb1c08a2b94a98
Eduardo Assis Tomich <29799589+eduardotomich@users.noreply.github.com>
2021-08-17 18:56:13 -0300

    Update Cliente.cpp
    defini um construtor para a classe

--- Cliente.cpp
+++ Cliente.cpp
@@ -2,6 +2,15 @@
 #include <string>
 #include "Cliente.hpp"
 
+Cliente::Cliente(string _nome, string _telefone, string _endereco, string _cidade, string _uf, string _cep){
+    this->nome = _nome;
+    this->telefone = _telefone; 
+    this->endereco = _endereco;
+    this->cidade = _cidade;
+    this->uf = _uf;
+    this->cep = _cep;
+}
+
 void Cliente::print(){
 
 	std::cout << "  Nome: " << nome << endl
