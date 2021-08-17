0278cd6629a45c23916248223eedbe1e5a632cc3
LucasGontijoC <89094649+LucasGontijoC@users.noreply.github.com>
2021-08-17 16:53:34 -0300

    Update Cliente.hpp
    -Alteração de public para private.

--- Cliente.hpp
+++ Cliente.hpp
@@ -5,15 +5,39 @@
 using namespace std;
 
 class Cliente {
+    
     public:
-        string NOME;
-        string endereco;
-        string CIDADE;
-        string UF;
-        string cep;
-        string telefone;
+        
+        Cliente();
+        Cliente(string _Nome, string _Endereco, string _Cidade, string _UF, string _Cep, string _Telefone);
 
+        void set_Nome(string _Nome);
+        void set_Endereco(string _Endereco);
+        void set_Cidade(string _Cidade);
+        void set_UF(string _UF);
+        void set_Cep(string _Cep);
+        void set_Telefone(string _Telefone);
+        
+        string get_Nome();
+        string get_Endereco();
+        string get_Cidade();
+        string get_UF();
+        string get_Cep();
+        string get_Telefone();
+    
+        
         void print();
+    
+    private:
+    
+        string Nome;
+        string Endereco;
+        string Cidade;
+        string UF;
+        string Cep;
+        string Telefone;
+
+    
 };
 
 #endif
