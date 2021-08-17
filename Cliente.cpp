69e53e9af66c0f821ace79ce2155e9f925f17dbb
Thiago Pádua <paduathiago@outlook.com>
2021-08-17 18:56:58 -0300

    Adicionei getters e setters

--- Cliente.cpp
+++ Cliente.cpp
@@ -4,11 +4,49 @@
 
 void Cliente::print(){
 
-	std::cout << "  Nome: " << NOME << endl
-		 << "  Telefone: " << telefone << endl
-		 << "  Endereço: " << endereco << endl
-		 << "  Cidade: " << CIDADE << endl
-		 << "  Estado: " << UF << endl
-		 << "  CEP: " << cep << endl;
+	std::cout << "  Nome: " << get_nome() << endl
+		 << "  Telefone: " << get_telefone() << endl
+		 << "  Endereço: " << get_endereco() << endl
+		 << "  Cidade: " << get_cidade << endl
+		 << "  Estado: " << get_uf << endl
+		 << "  CEP: " << get_cep << endl;
 
+}
+
+string Cliente::get_nome(){
+	return _nome;
+}
+string Cliente::get_telefone(){
+	return _telefone;
+}
+string Cliente::get_endereco(){
+	return _endereco;
+}
+string Cliente::get_cidade(){
+	return _cidade;
+}
+string Cliente::get_uf(){
+	return _uf;
+}
+string Cliente::get_cep(){
+	return _cep;
+}
+
+void Cliente::set_nome(string nome){
+	_nome = nome;
+}
+void Cliente::set_endereco(string endereco){
+	_endereco = endereco;
+}
+void Cliente::set_cidade(string cidade){
+	_cidade = cidade;
+}
+void Cliente::set_uf(string uf){
+	_uf = uf;
+}
+void Cliente::set_cep(string cep){
+	_cep = cep;
+}
+void Cliente::set_telefone(string telefone){
+	_telefone = telefone;
 }
