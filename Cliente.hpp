878dffc6f97f34464c507a947837ec6df5bcf506
Thiago Pádua <paduathiago@outlook.com>
2021-08-17 18:59:22 -0300

    Adicionei construtores, getters, setters e apliquei encapsulamento nos atributos

--- Cliente.hpp
+++ Cliente.hpp
@@ -6,14 +6,31 @@ using namespace std;
 
 class Cliente {
     public:
-        string NOME;
-        string endereco;
-        string CIDADE;
-        string UF;
-        string cep;
-        string telefone;
+        Cliente(string nome, string endereco, string cidade, string uf, string cep, string telefone);
 
         void print();
+        
+        string get_nome();
+        string get_endereco();
+        string get_cidade();
+        string get_uf();
+        string get_cep();
+        string get_telefone();
+
+        void set_nome(string nome);
+        void set_endereco(string endereco);
+        void set_cidade(string cidade);
+        void set_uf(string uf);
+        void set_cep(string cep);
+        void set_telefone(string telefone);
+
+    private:
+        string _nome;
+        string _endereco;
+        string _cidade;
+        string _uf;
+        string _cep;
+        string _telefone;
 };
 
 #endif
