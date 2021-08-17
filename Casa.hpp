8c612d10e0e9ef21574d3cc6369cb16f2c93922b
ReiNicolas <88855197+ReiNicolas@users.noreply.github.com>
2021-08-17 10:17:25 -0300

    Update Casa.hpp

--- Casa.hpp
+++ Casa.hpp
@@ -22,20 +22,6 @@ class Casa : public Imovel {
     
     Casa(double _Area, int _Quartos, int _Banheiros, int _Vagas, double _VALORm2, string _Corretor, Cliente _Vendedor): Imovel(double _Area, int _Quartos, int _Banheiros, int _Vagas, double _VALORm2, string _Corretor, Cliente _Vendedor){
     };
-        
-    void print() {
-
-        std::cout << "[Casa]" << endl;
-        Imovel::print();
-        std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
-                << "Taxa de Comissão: " << 6 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
-                << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
-
-    }
 
 };
 
