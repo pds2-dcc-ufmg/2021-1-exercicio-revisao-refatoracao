abdea329d1c8b48be820ed8c7e6cb2cbe0605714
Bilacomy <87915087+Bilacomy@users.noreply.github.com>
2021-08-17 14:58:38 -0300

    Update Casa.cpp

--- Casa.cpp
+++ Casa.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <string>
+#include "Casa.hpp"
 
+double comissao() {
+        
+double comissao = area * valor_m2;
+	return comissao * 0.06;  
+}
+
+void print() {
+
+	std::cout << "[Casa]" << endl;
+	Imovel::print();
+	std::cout << "Area: " << area << endl
+              << "  Quartos: " << quartos << endl
+              << "  Banheiros: " << banheiros << endl
+              << "  Vagas: " << vagas << endl
+              << "Taxa de Comissão: " << 6 << "%" << endl
+              << "Valor Comissão: R$ " << fixed << setprecision(2) << comissao << endl
+              << "Valor de Venda: R$ " << fixed << setprecision(2) << valor << endl;
+}
