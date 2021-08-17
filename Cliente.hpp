a6157c83e4d9fcae3fcb889da130a1b5d653c6cb
lgmufmg <86167093+lgmufmg@users.noreply.github.com>
2021-08-17 15:12:07 -0300

    Update Cliente.hpp
    - Atributos do Cliente foram colocados como private, para maior segurança dos dados.
    - Adição de métodos set e get que viabilizam o item anterior.

--- Cliente.hpp
+++ Cliente.hpp
@@ -5,15 +5,33 @@
 using namespace std;
 
 class Cliente {
-    public:
+    private:
         string NOME;
-        string endereco;
+        string ENDERECO;
         string CIDADE;
         string UF;
-        string cep;
-        string telefone;
+        string CEP;
+        string TELEFONE;
 
-        void print();
+    public:
+         Cliente();
+         Cliente(string _NOME, string _ENDERECO, string _CIDADE, string _UF, string _CEP, string _TELEFONE);
+
+         void setNOME(string _NOME);
+         void setENDERECO(string _ENDERECO);
+         void setCIDADE(string _CIDADE);
+         void setUF(string _UF);
+         void setCEP(string _CEP);
+         void setTELEFONE(string _TELEFONE);
+    
+         string getNOME();
+         string getENDERECO();
+         string getCIDADE();
+         string getUF();
+         string getCEP();
+         string getTELEFONE();
+    
+         void print();
 };
 
 #endif
