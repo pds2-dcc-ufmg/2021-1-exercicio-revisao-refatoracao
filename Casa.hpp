cc44ee1a3229d81cca8a945e1e09440810494aff
barbararamos <68672159+barbararamos@users.noreply.github.com>
2021-08-17 21:45:45 -0300

    Update Casa.hpp

--- Casa.hpp
+++ Casa.hpp
@@ -31,9 +31,9 @@ class Casa : public Imovel {
         std::cout << "[Casa]" << endl;
         Imovel::print();
         std::cout << "Area: " << Area << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
+                << "Quartos: " << Q << endl
+                << "Banheiros: " << B << endl
+                << "Vagas: " << V << endl
                 << "Taxa de Comissão: " << 6 << "%" << endl
                 << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
                 << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
