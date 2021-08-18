b10f949af00012d3228bf3f15d0d99ea14f6309e
nathalialoliveira <79007746+nathalialoliveira@users.noreply.github.com>
2021-08-17 23:10:43 -0300

    Separação do hpp e cpp e definindo cte

--- Casa.hpp
+++ Casa.hpp
@@ -1,5 +1,6 @@
 #ifndef CASA_HPP
 #define CASA_HPP
+#define taxaCom 0.06
 
 #include "Imovel.hpp"
 #include "Cliente.hpp"
@@ -10,36 +11,12 @@ class Casa : public Imovel {
 
     public:
 
-    double valor() {
-
-        double v = AREA * VALORm2;
-
-        return v;
-
-    }
-
-    double comissao() {
-
-        double c = AREA * VALORm2;
-
-        return c * 0.06;
-        
-    }
-
-    void print() {
-
-        std::cout << "[Casa]" << endl;
-        Imovel::print();
-        std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
-                << "Taxa de Comissão: " << 6 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
-                << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
-
-    }
-
+    double valor();
+    double ValorCasa;
+    
+    double comissao();
+    
+    void print();
 };
 
 #endif
