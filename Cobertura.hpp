c844c7c7375d4ac503ec24826d260e7a9f11ee6b
barbararamos <68672159+barbararamos@users.noreply.github.com>
2021-08-17 21:36:33 -0300

    Update Cobertura.hpp

--- Cobertura.hpp
+++ Cobertura.hpp
@@ -30,9 +30,9 @@ class Cobertura : private Imovel {
         std::cout << "[Cobertura]" << endl;
         Imovel::print();
         std::cout << "Area: " << Area << endl
-                << "  Quartos: " << Quartos << endl
-                << "  Banheiros: " << Banheiros << endl
-                << "  Vagas: " << Vagas << endl
+                << "Quartos: " << Quartos << endl
+                << "Banheiros: " << Banheiros << endl
+                << "Vagas: " << Vagas << endl
                 << "Taxa de Comissão: " << 10 << "%" << endl
                 << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
                 << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
