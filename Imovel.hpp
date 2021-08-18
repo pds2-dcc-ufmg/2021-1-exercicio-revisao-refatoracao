bc2589c864435edfec9c52e9c92f02c7139f7f54
Arthur Fernandes <arthursfernandes13@gmail.com>
2021-08-17 23:09:44 -0300

    Update Imovel.hpp

--- Imovel.hpp
+++ Imovel.hpp
@@ -2,29 +2,31 @@
 #define IMOVEL_HPP
 
 #include <iostream>
+#include <vector>
+#include "ImovelProps.hpp"
 #include <iomanip>
 #include "Cliente.hpp"
 
 using namespace std;
 
 class Imovel {
-    public:
-        double AREA;
-        int Q;
-        int B;
-        int V;
-        double VALORm2;
-        double Valor;
-        double C;
-        Cliente vendedor;
-        string corretor;
+    
+    protected:
+    ImovelProps _props;
+    void printTituloImovel ();
+    void printImovel ();
 
-        void print() {
-            cout << "[Vendedor]" << endl;
-            vendedor.print();
-            cout << "[Corretor]" << endl;
-            cout << "  " + corretor << endl;
-        }
+     public:
+    Imovel(ImovelProps props) : _props(props) {};
+
+    virtual void print () {};
+
+    ImovelProps getProps () const { return _props; }
+
+    double getValor () const { return _props.area * _props.valorM2; }
+    virtual double getTaxaComissao () const { return 0.0; };
+    double getComissao () const { return getTaxaComissao() * getValor(); };
+    double getValorTotal () const { return getValor() + getComissao(); };
 };
 
 #endif
