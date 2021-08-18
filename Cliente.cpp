7c801d4308175993a5d64484ebf2c7e4aed1be8a
TainaraTeixeira09 <89044610+TainaraTeixeira09@users.noreply.github.com>
2021-08-17 23:49:18 -0300

    bb832437c394950b3b9e49ea7ffaabf7
    Definição da função print como override

--- Cliente.cpp
+++ Cliente.cpp
@@ -12,3 +12,13 @@ void Cliente::print(){
 		 << "  CEP: " << cep << endl;
 
 }
+
+void Cliente::cadastrar(std::string NOME, string endereco, string CIDADE, string UF, string cep, string telefone)
+{
+	this->NOME = NOME;
+	this->endereco = endereco;
+	this->CIDADE = CIDADE;
+	this->UF = UF;
+	this->cep = cep;
+	this->telefone = telefone;
+}
