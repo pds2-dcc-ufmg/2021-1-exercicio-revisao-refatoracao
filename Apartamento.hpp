9dfef9a67c4abbfdb58e200c2ad45c0ee3e6dbd0
barbararamos <68672159+barbararamos@users.noreply.github.com>
2021-08-17 21:46:28 -0300

    Update Apartamento.hpp

--- Apartamento.hpp
+++ Apartamento.hpp
@@ -12,7 +12,7 @@ class Apartamento : public Imovel {
     
     double valor() {
 
-        double v = AREA * VALORm2;
+        double v = Area * Valorm2;
 
         return v;
 
@@ -20,7 +20,7 @@ class Apartamento : public Imovel {
 
     double comissao() {
 
-        double c = AREA * VALORm2;
+        double c = Area * Valorm2;
 
         return c * 0.04;
         
@@ -31,9 +31,9 @@ class Apartamento : public Imovel {
         std::cout << "[Apartamento]" << endl;
         Imovel::print();
         std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
+                << "Quartos: " << Q << endl
+                << "Banheiros: " << B << endl
+                << "Vagas: " << V << endl
                 << "Taxa de Comissão: " << 4 << "%" << endl
                 << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
                 << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
