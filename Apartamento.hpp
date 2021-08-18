86a72fc02aeb31f3af6eaf435ca78d5b94f356f0
Eduardo Assis Tomich <29799589+eduardotomich@users.noreply.github.com>
2021-08-17 22:09:11 -0300

    Create Apartamento.hpp
    Nomes das variáveis alterados para nomes mais significativos em lowercase com underline 
    Formatação e identação
    Remoção do Numero Mágico 4
    removidos os metodos de getValor e getComissao que agora fazem parte da superclasse

--- Apartamento.hpp
+++ Apartamento.hpp
@@ -1,5 +1,7 @@
 #ifndef APARTAMENTO_HPP
 #define APARTAMENTO_HPP
+#define COMISSAO 0.4
+#define PERCENT 100
 
 #include "Imovel.hpp"
 #include "Cliente.hpp"
@@ -8,10 +10,22 @@ using namespace std;
 
 class Apartamento : public Imovel {
 
-    public:
-        
-        Apartamento(double _area, int _n_quartos, int _n_banheiros, int _n_vagas, double _valor_m2, double _valor_total, double _comissao, Cliente _vendedor, string _corretor);
+        public:
+    void print() {
 
-        void print();
+        std::cout << "[Apartamento]" << endl;
+        Imovel::print();
+        std::cout << "Area: " << area << endl
+                << "  Quartos: " << n_quartos << endl
+                << "  Banheiros: " << n_banheiros << endl
+                << "  Vagas: " << n_vagas << endl
+                << "Taxa de Comissão: " << COMISSAO * PERCENT << "%" << endl
+                << "Valor Comissão: R$ " << fixed << setprecision(2) << comissao << endl
+                << "Valor de Venda: R$ " << fixed << setprecision(2) << valor_total << endl;
+    }
+        private:
+    
+    this->comissao = COMISSAO;
 };
+
 #endif
