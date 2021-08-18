ec8e4b998cea22873a95493c70ee669d0dccb383
TainaraTeixeira09 <89044610+TainaraTeixeira09@users.noreply.github.com>
2021-08-17 23:53:16 -0300

    bb832437c394950b3b9e49ea7ffaabf7
    Definição da função print como override

--- Casa.hpp
+++ Casa.hpp
@@ -1,21 +1,19 @@
-#ifndef CASA_HPP
-#define CASA_HPP
+#ifndef COBERTURA_HPP
+#define COBERTURA_HPP
 
 #include "Imovel.hpp"
 #include "Cliente.hpp"
-
 using namespace std;
 
-class Casa : public Imovel
+class Cobertura : public Imovel
 {
 
 public:
     double valor();
+
     double comissao();
-    
-    /* Método print recebeu override pois foi herdado da classe mãe. */
+
     void print() override;
-    
 };
 
 #endif
