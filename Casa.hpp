aebe7ddfdbbe7e27dcf7041edc817bb18c4db647
TainaraTeixeira09 <89044610+TainaraTeixeira09@users.noreply.github.com>
2021-08-17 23:49:23 -0300

    bb832437c394950b3b9e49ea7ffaabf7
    Definição da função print como override

--- Casa.hpp
+++ Casa.hpp
@@ -6,40 +6,16 @@
 
 using namespace std;
 
-class Casa : public Imovel {
+class Casa : public Imovel
+{
 
-    public:
-
-    double valor() {
-
-        double v = AREA * VALORm2;
-
-        return v;
-
-    }
-
-    double comissao() {
-
-        double c = AREA * VALORm2;
-
-        return c * 0.06;
-        
-    }
-
-    void print() {
-
-        std::cout << "[Casa]" << endl;
-        Imovel::print();
-        std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
-                << "Taxa de Comissão: " << 6 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
-                << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
-
-    }
-
+public:
+    double valor();
+    double comissao();
+    
+    /* Método print recebeu override pois foi herdado da classe mãe. */
+    void print() override;
+    
 };
 
 #endif
