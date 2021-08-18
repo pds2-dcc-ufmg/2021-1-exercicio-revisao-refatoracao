0fbffa70937e05f419f24ccbd5fc428a5ccdd271
FredericoFerri <61014235+FredericoFerri@users.noreply.github.com>
2021-08-17 22:51:41 -0300

    Update Casa.hpp

--- Casa.hpp
+++ Casa.hpp
@@ -12,7 +12,7 @@ class Casa : public Imovel {
 
     double valor() {
 
-        double v = AREA * VALORm2;
+        double v = area * valor_m2;
 
         return v;
 
@@ -20,7 +20,7 @@ class Casa : public Imovel {
 
     double comissao() {
 
-        double c = AREA * VALORm2;
+        double c = area * valor_m2;
 
         return c * 0.06;
         
@@ -30,13 +30,13 @@ class Casa : public Imovel {
 
         std::cout << "[Casa]" << endl;
         Imovel::print();
-        std::cout << "Area: " << AREA << endl
+        std::cout << "Area: " << area << endl
                 << "  Quartos: " << Q << endl
                 << "  Banheiros: " << B << endl
                 << "  Vagas: " << V << endl
                 << "Taxa de Comissão: " << 6 << "%" << endl
                 << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
-                << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
+                << "Valor de Venda: R$ " << fixed << setprecision(2) << valor << endl;
 
     }
 
