3ba807a9804a4f70bdb770790322176d3c049e36
jorgelacerda <55405305+jorgelacerda@users.noreply.github.com>
2021-08-17 23:01:43 -0300

    Descrição detalhada e Reorganização do código

--- Imovel.hpp
+++ Imovel.hpp
@@ -9,15 +9,16 @@ using namespace std;
 
 class Imovel {
     public:
-        double AREA;
-        int Q;
-        int B;
-        int V;
-        double VALORm2;
-        double Valor;
-        double C;
+        double AREA; // Variavel que representa a Area do Imovel
+        int Q; //Variável que representa o número de QUARTOS em um Imovel
+        int B; //Variável que representa o número de BANHEIROS em um Imovel
+        int V; //Variável que representa o número de VAGAS em um Imovel
+        double VALORm2; //Variável que representa o valor por metro quadrado de um Imovel
+        double Valor; //Variável que representa o Valor de venda um Imovel
+        double C; //Variável que representa o valor da comissão do corretor ao vender um Imovel
         Cliente vendedor;
         string corretor;
+
 
         void print() {
             cout << "[Vendedor]" << endl;
