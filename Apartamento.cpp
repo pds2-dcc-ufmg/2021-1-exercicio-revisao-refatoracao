960377372427f74702e1a619469353c546128859
nathalialoliveira <79007746+nathalialoliveira@users.noreply.github.com>
2021-08-17 22:58:36 -0300

    implementação do header

---
+++ Apartamento.cpp
@@ -0,0 +1,25 @@
+#include "Apartamento.hpp"
+#include<iostream>
+#include<string>
+using namespace std;
+
+double Apartamento::valor(){
+    ValorApt = Area*Valorm2;
+    return ValorApt;
+}
+double Apartamento::comissao(){
+    ValorApt = Area*Valorm2;
+    return ValorApt*taxaApt;
+}
+void Apartamento::print(){
+
+    std::cout << "[Apartamento]" << endl;
+    Imovel::print();
+    std::cout   << "Area: " << Area << endl
+                << "  Quartos: " << Quartos << endl
+                << "  Banheiros: " << Banheiros << endl
+                << "  Vagas: " << Vagas << endl
+                << "Taxa de Comissão: " << 4 << "%" << endl
+                << "Valor Comissão: R$ " << fixed << setprecision(2) << _Comissao << endl
+                << "Valor de Venda: R$ " << fixed << setprecision(2) << _ValorImovel << endl;
+    }
