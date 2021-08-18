d305ae88802a2bbedf34ca2a83128fd70554c5d1
LucasGontijoC <89094649+LucasGontijoC@users.noreply.github.com>
2021-08-17 21:37:29 -0300

    Update Cobertura.hpp

--- Cobertura.hpp
+++ Cobertura.hpp
@@ -7,25 +7,13 @@ using namespace std;
 
 class Cobertura : public Imovel {
     
-    public:
-
-    double comissao() {
-
-        return Valor_Imovel * Perc_Comissao_Cobert;        
-    }
-
-    void print() {
-
-        std::cout << "[Cobertura]" << endl;
-        Imovel::print();
-        std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Qnt_Quartos << endl
-                << "  Banheiros: " << Qnt_Banheiros << endl
-                << "  Vagas: " << Qnt_Vagas << endl
-                << "Taxa de Comissão: " << (Perc_Comissao_Cobert * 100) << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << comissao << endl
-                << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor_Venda << endl;
-    }
+public:
+    Cobertura(string corretor, double AREA, int Quartos, int Banheiros, int Vagas, double VALORm2, Cliente vendedor);
+    double Perc_Comissao_Cbrt = 0.1;
+    double valor();
+    double comissao();
+    void print();
 };
 
 #endif
+
