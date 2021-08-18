3217fea636b41440274972b611f755bbc7058040
BellaVignoli <bellavignoli@gmail.com>
2021-08-17 22:53:04 -0300

    Alterações arquivo Casa.hpp

--- Casa.hpp
+++ Casa.hpp
@@ -4,40 +4,22 @@
 #include "Imovel.hpp"
 #include "Cliente.hpp"
 
-using namespace std;
-
 class Casa : public Imovel {
 
     public:
 
-    double valor() {
-
-        double v = AREA * VALORm2;
-
-        return v;
-
+    double valor(){
+        return Imovel::valor();
     }
 
     double comissao() {
-
-        double c = AREA * VALORm2;
-
-        return c * 0.06;
-        
+        return Imovel::comissao();
     }
 
-    void print() {
+    void Impressao_Imovel() {
 
         std::cout << "[Casa]" << endl;
-        Imovel::print();
-        std::cout << "Area: " << AREA << endl
-                << "  Quartos: " << Q << endl
-                << "  Banheiros: " << B << endl
-                << "  Vagas: " << V << endl
-                << "Taxa de Comissão: " << 6 << "%" << endl
-                << "Valor Comissão: R$ " << fixed << setprecision(2) << C << endl
-                << "Valor de Venda: R$ " << fixed << setprecision(2) << Valor << endl;
-
+        Imovel::Impressao_Imovel();
     }
 
 };
