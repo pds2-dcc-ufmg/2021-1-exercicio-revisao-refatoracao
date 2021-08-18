33c0963e715916bfbc5d6ba58d586a267062f463
FredericoFerri <61014235+FredericoFerri@users.noreply.github.com>
2021-08-17 23:12:46 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include "Apartamento.hpp"
 #include "Cliente.hpp"
 
+template <class T> 
+
 int main() {
     
     Cliente cl1 ("Xayso Sovon Ziahaka", "Rua Xangrilá - Braúnas",
@@ -39,99 +41,99 @@ int main() {
 
     Apartamento ap1;
     ap1.corretor = "Tuoruars";
-    ap1.AREA = 55.4;
+    ap1.area = 55.4;
     ap1.Q = 2;
     ap1.B = 1;
     ap1.V = 0;
-    ap1.VALORm2 = 987.0;
+    ap1.valor_m2 = 987.0;
     ap1.C = ap1.comissao();
-    ap1.Valor = ap1.valor() + ap1.comissao();
+    ap1.valor = ap1.valor() + ap1.comissao();
     ap1.vendedor = cl1;
     qtdeA++;
 
     Apartamento ap2;
     ap2.corretor = "Fyubyeis";
-    ap2.AREA = 74.5;
+    ap2.area = 74.5;
     ap2.Q = 2;
     ap2.B = 1;
     ap2.V = 2;
-    ap2.VALORm2 = 1540.0;
+    ap2.valor_m2 = 1540.0;
     ap2.C = ap2.comissao();
-    ap2.Valor = ap2.valor() + ap2.comissao();
+    ap2.valor = ap2.valor() + ap2.comissao();
     ap2.vendedor = cl2;
     qtdeA++;
 
     Apartamento ap3;
     ap3.corretor = "Kelia";
-    ap3.AREA = 87.2;
+    ap3.area = 87.2;
     ap3.Q = 3;
     ap3.B = 2;
     ap3.V = 2;
-    ap3.VALORm2 = 2354.0;
+    ap3.valor_m2 = 2354.0;
     ap3.C = ap3.comissao();
-    ap3.Valor = ap3.valor() + ap3.comissao();
+    ap3.valor = ap3.valor() + ap3.comissao();
     ap3.vendedor = cl3;
     qtdeA++;
 
     // Cobertura
     Cobertura cb1;
     cb1.corretor = "Koci";
-    cb1.AREA = 120.1;
+    cb1.area = 120.1;
     cb1.Q = 3;
     cb1.B = 3;
     cb1.V = 2;
-    cb1.VALORm2 = 3123.5;
+    cb1.valor_m2 = 3123.5;
     cb1.C = cb1.comissao();
-    cb1.Valor = cb1.valor() + cb1.comissao();
+    cb1.valor = cb1.valor() + cb1.comissao();
     cb1.vendedor = cl4;
     qtdeCb++;
     
     Cobertura cb2;
     cb2.corretor = "Wail";
-    cb2.AREA = 134.8;
+    cb2.area = 134.8;
     cb2.Q = 4;
     cb2.B = 3;
     cb2.V = 3;
-    cb2.VALORm2 = 3578.2;
+    cb2.valor_m2 = 3578.2;
     cb2.C = cb2.comissao();
-    cb2.Valor = cb2.valor() + cb2.comissao();
+    cb2.valor = cb2.valor() + cb2.comissao();
     cb2.vendedor = cl5;
     qtdeCb++;
 
     Cobertura cb3;
     cb3.corretor = "Fival";
-    cb3.AREA = 180.0;
+    cb3.area = 180.0;
     cb3.Q = 4;
     cb3.B = 4;
     cb3.V = 4;
-    cb3.VALORm2 = 4165.7;
+    cb3.valor_m2 = 4165.7;
     cb3.C = cb3.comissao();
-    cb3.Valor = cb3.valor() + cb3.comissao();
+    cb3.valor = cb3.valor() + cb3.comissao();
     cb3.vendedor = cl6;
     qtdeCb++;
 
     // Casas
     Casa ca1;
     ca1.corretor = "Beydo";
-    ca1.AREA = 145.6;
+    ca1.area = 145.6;
     ca1.Q = 3;
     ca1.B = 3;
     ca1.V = 2;
-    ca1.VALORm2 = 4023.6;
+    ca1.valor_m2 = 4023.6;
     ca1.C = ca1.comissao();
-    ca1.Valor = ca1.valor() + ca1.comissao();
+    ca1.valor = ca1.valor() + ca1.comissao();
     ca1.vendedor = cl7;
     qtdeCa++;
 
     Casa ca2;
     ca2.corretor = "Riuzi";
-    ca2.AREA = 245.0;
+    ca2.area = 245.0;
     ca2.Q = 5;
     ca2.B = 4;
     ca2.V = 4;
-    ca2.VALORm2 = 4856.2;
+    ca2.valor_m2 = 4856.2;
     ca2.C = ca2.comissao();
-    ca2.Valor = ca2.valor() + ca2.comissao();
+    ca2.valor = ca2.valor() + ca2.comissao();
     ca2.vendedor = cl8;
     qtdeCa++;
 
@@ -144,46 +146,22 @@ int main() {
     double comissCb = 0.0;
     double comissCa = 0.0;
     
-    ap1.print();
-    valorAp += ap1.valor();
-    comissAp += ap1.comissao();
-    std::cout << endl;
-
-    ap2.print();
-    valorAp += ap2.valor();
-    comissAp += ap2.comissao();
-    std::cout << endl;
-
-    ap3.print();
-    valorAp += ap3.valor();
-    comissAp += ap3.comissao();
-    std::cout << endl;
-
-    cb1.print();
-    valorCb += cb1.valor();
-    comissCb += cb1.comissao();
-    std::cout << endl;
-
-    cb2.print();
-    valorCb += cb2.valor();
-    comissCb += cb2.comissao();
-    std::cout << endl;
-
-    cb3.print();
-    valorCb += cb3.valor();
-    comissCb += cb3.comissao();
-    std::cout << endl;
-
-    ca1.print();
-    valorCa += ca1.valor();
-    comissCa += ca1.comissao();
-    std::cout << endl;
-
-    ca2.print();
-    valorCa += ca2.valor();
-    comissCa += ca2.comissao();
-    std::cout << endl;
-
+    ap1.procedure(&valorAp, &comissAp);
+        
+    ap2.procedure(&valorAp, &comissAp);
+    
+    ap3.procedure(&valorAp, &comissAp);
+    
+    cb1.procedure(&valorCb, &comissCb);
+    
+    cb2.procedure(&valorCb, &comissCb);
+    
+    cb3.procedure(&valorCb, &comissCb);
+    
+    ca1.procedure(&valorCa, &comissCa);
+    
+    ca2.procedure(&valorCa, &comissCa);
+    
     std::cout << "\n>> Resumo Geral <<" << endl;
 
     std::cout << "\n>>Apartamentos<<\n"
